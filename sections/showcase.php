52d6779f2cf6e0beec4372015d9b53a937938805
Kevin DuCommun <kevin@rockettheme.com>
2014-02-10 23:28:03 -0600

    button modifier integration, mixin consolidation

--- sections/showcase.php
+++ sections/showcase.php
@@ -12,7 +12,7 @@
 				<div class="content">
 					<h2>Gantry Flex Grid</h2>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies tristique nulla et mattis. Phasellus id massa eget nisl congue blandit sit amet id ligula. Praesent et nulla eu augue tempus sagittis. Mauris faucibus nibh et nibh cursus in vestibulum sapien egestas.</p>
-					<a href="#" class="button">Button</a>
+					<a href="#" class="button-secondary">Button</a>
 				</div>
 			</div>
 			<div class="block">
