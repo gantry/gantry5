aa8168b37d807e2d9be6bcdcc4a205c8d4aa9c2f
Kevin DuCommun <kevin@rockettheme.com>
2014-02-12 14:57:27 -0600

    added responsive support for devices, cleanup

--- core/head.php
+++ core/head.php
@@ -1,6 +1,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Flexbox Grid</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css-compiled/core.css" type="text/css" />
 	<!--[if IE 9]>
 		<link rel="stylesheet" href="css/core-ie9.css" type="text/css" />
