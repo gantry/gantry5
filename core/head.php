0ae47f91e97efc7c07cbb6953b2e7d646ea8c1b2
Kevin DuCommun <kevin@rockettheme.com>
2014-02-13 11:43:11 -0600

    updates for buttons and typography

--- core/head.php
+++ core/head.php
@@ -1,6 +1,6 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Flexbox Grid</title>
+	<title>Gantry Playground</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css-compiled/core.css" type="text/css" />
 	<!--[if IE 9]>
