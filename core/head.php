158dbc96704f77503c5992c7573ee3cfe79e2e13
Kevin DuCommun <kevin@rockettheme.com>
2014-04-08 18:56:24 -0500

    added nucleus integration, core rework

--- core/head.php
+++ core/head.php
@@ -2,6 +2,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Gantry Playground</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css-compiled/nucleus.css" type="text/css" />
 	<link rel="stylesheet" href="css-compiled/core.css" type="text/css" />
 	<!--[if IE 9]>
 		<link rel="stylesheet" href="css/core-ie9.css" type="text/css" />
