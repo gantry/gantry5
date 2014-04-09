8355c3b1025fae583e48a1639d765d6642dbb1c8
Kevin DuCommun <kevin@rockettheme.com>
2014-04-08 19:14:59 -0500

    more core rework, new directory structure and naming

--- core/head.php
+++ core/head.php
@@ -3,9 +3,9 @@
 	<title>Gantry Playground</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css-compiled/nucleus.css" type="text/css" />
-	<link rel="stylesheet" href="css-compiled/core.css" type="text/css" />
+	<link rel="stylesheet" href="css-compiled/template.css" type="text/css" />
 	<!--[if IE 9]>
-		<link rel="stylesheet" href="css/core-ie9.css" type="text/css" />
+		<link rel="stylesheet" href="css/nucleus-ie9.css" type="text/css" />
 		<link rel="stylesheet" href="css/pure/grids-min.css" type="text/css" />
 	<![endif]-->
 </head>
