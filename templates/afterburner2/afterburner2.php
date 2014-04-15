134ed4d0a0cf4a4a9e950bf6ceb4d92f60a5338d
Kevin DuCommun <kevin@rockettheme.com>
2014-04-15 13:09:11 -0500

    afterburner2 test template updates, nucleus updates for bootstrap inheritance

--- templates/afterburner2/afterburner2.php
+++ templates/afterburner2/afterburner2.php
@@ -24,7 +24,7 @@
 						</div>
 					</div>
 					<div class="block pure-2-3">
-						<div class="content">
+						<div class="content menu-block">
 							<ul class="gf-menu l1 "> <li class="item101 active last"> <a class="item" href="/live/joomla/afterburner2/"> Home            </a> </li> <li class="item115 parent"> <a class="item" href="javascript:void(0);"> Features            </a> <div class="dropdown " style="width:180px;"> <div class="column"> <ul class="level2"> <li class="item104"> <a class="item" href="/live/joomla/afterburner2/features/module-variations"> Module Variations            </a> </li> <li class="item116"> <a class="item" href="/live/joomla/afterburner2/features/module-positions"> Module Positions            </a> </li> <li class="item117 parent"> <a class="item" href="javascript:void(0);"> Menu Example            </a> <div class="dropdown flyout " style="width:180px;"> <div class="column"> <ul class="level3"> <li class="item121"> <a class="item" href="javascript:void(0);"> Child Item            </a> </li> <li class="item122"> <a class="item" href="javascript:void(0);"> Child Item            </a> </li> <li class="item123"> <a class="item" href="javascript:void(0);"> Child Item            </a> </li> <li class="item124"> <a class="item" href="javascript:void(0);"> Child Item            </a> </li> </ul> </div> </div> </li> </ul> </div> </div> </li> <li class="item103"> <a class="item" href="/live/joomla/afterburner2/typography"> Typography            </a> </li> <li class="item105 parent"> <a class="item" href="/live/joomla/afterburner2/joomla"> Joomla            </a> <div class="dropdown " style="width:180px;"> <div class="column"> <ul class="level2"> <li class="item106"> <a class="item" href="/live/joomla/afterburner2/joomla/category-blog"> Category Blog            </a> </li> <li class="item107"> <a class="item" href="/live/joomla/afterburner2/joomla/member-login"> Member Login            </a> </li> <li class="item108"> <a class="item" href="/live/joomla/afterburner2/joomla/web-links"> Web Links            </a> </li> </ul> </div> </div> </li> <li class="item110 parent"> <a class="item" href="/live/joomla/afterburner2/presets"> Presets            </a> <div class="dropdown " style="width:180px;"> <div class="column"> <ul class="level2"> <li class="item111"> <a class="item" href="/live/joomla/afterburner2/?presets=preset1"> Preset 1            </a> </li> <li class="item112"> <a class="item" href="/live/joomla/afterburner2/?presets=preset2"> Preset 2            </a> </li> <li class="item113"> <a class="item" href="/live/joomla/afterburner2/?presets=preset3"> Preset 3            </a> </li> </ul> </div> </div> </li> </ul>
 						</div>
 					</div>
@@ -46,7 +46,7 @@
 					</div>
 				</div>
 				<div class="grid pure-g-r">
-					<div class="block pure-u-1-3">
+					<div class="block pure-u-1-3 promo">
 						<div class="content">
 							<div class="grid pure-g-r">
 								<div class="block size-1-3 pure-u-1-3">     
@@ -61,7 +61,7 @@
 						</div>
 					</div>
 
-					<div class="block pure-u-1-3">
+					<div class="block pure-u-1-3 promo">
 						<div class="content">
 							<div class="grid pure-g-r">
 								<div class="block size-1-3 pure-u-1-3">        
@@ -76,7 +76,7 @@
 						</div>
 					</div>
 
-					<div class="block pure-u-1-3">
+					<div class="block pure-u-1-3 promo">
 						<div class="content">
 							<div class="grid pure-g-r">
 								<div class="block size-1-3 pure-u-1-3">       
@@ -100,47 +100,46 @@
 				<div class="block pure-u-2-3">
 					<section id="main">
 						<div class="content">
-							<h2 class="title">Popular Features</h3>
-							<div class="rt-relative largemarginbottom  largepaddingbottom largepaddingright rt-border-right rt-border-bottom">
+							<h2 class="title">Popular Features</h2>
+							<div class="largemarginbottom largepaddingtop largepaddingbottom rt-border-bottom">
 								<div class="rt-big-icon rt-blue fa fa-cogs"></div>
 								<h5 class="smallmarginbottom nomargintop">Framework</h5>
 								<em><small>Advanced Features</small></em>
 								<p>Gantry provides the template foundation, with its standard set of features, such as the advanced template manager.</p>
-								<p><a href="http://gantry-framework.org/" class="readon">Read More</a></p>
+								<p><a href="#" class="readon">Read More</a></p>
 							</div>
-
-							<h2 class="title">Template Extras</h3>
+							<h2 class="title">Template Extras</h2>
 							<div class="grid pure-g-r">
-								<div class="block pure-u-1-2"><div class="gantry-width-spacer">
-									<div class="rt-relative largemarginbottom largepaddingright rt-border-right rt-border-bottom">
-										<p>Afterburner2 is an experiment in how streamlined a Gantry template can be whilst still being stunning and usable. Many extras have been removed, but still contains styling for major elements.</p>
+								<div class="block pure-u-1-2">
+									<div class="largepaddingtop largepaddingright largemarginright largemarginbottom">
+									Afterburner2 is an experiment in how streamlined a Gantry template can be whilst still being stunning and usable. Many extras have been removed, but still contains styling for major elements.
 									</div>
-								</div></div>
+								</div>
 
-								<div class="block pure-u-1-2"><div class="gantry-width-spacer">
-									<div class="rt-relative largemarginbottom largepaddingright rt-border-right rt-border-bottom">
+								<div class="block pure-u-1-2">
+									<div class="largepaddingtop largepaddingright largemarginright largemarginbottom">
 										<div class="rt-small-icon rt-red fa fa-tasks"></div>
-										<h5 class="smallmarginbottom nomargintop">Dropdowns</h5>
+										<h5 class="nomargintop">Dropdowns</h5>
 										<p>A CSS based dropdown menu system.</p>
 									</div>
-								</div></div>
+								</div>
 							</div>
 							<div class="grid pure-g-r">
-								<div class="block pure-u-1-2"><div class="gantry-width-spacer">
-									<div class="rt-relative largemarginbottom largepaddingright rt-border-right">
+								<div class="block pure-u-1-2">
+									<div class="largepaddingtop largepaddingright largemarginright largemarginbottom">
 										<div class="rt-small-icon rt-grey fa fa-list"></div>
-										<h5 class="smallmarginbottom nomargintop">Typography</h5>
+										<h5 class="nomargintop">Typography</h5>
 										<p>Elements are styled by a streamlined version of Bootstrap.</p>
 									</div>
-								</div></div>
+								</div>
 
-								<div class="block pure-u-1-2"><div class="gantry-width-spacer">
-									<div class="rt-relative largemarginbottom largepaddingright rt-border-right">
+								<div class="block pure-u-1-2">
+									<div class="largepaddingtop largepaddingright largemarginright largemarginbottom">
 										<div class="rt-small-icon rt-blue fa fa-edit"></div>
-										<h5 class="smallmarginbottom nomargintop">Optimized</h5>
+										<h5 class="nomargintop">Optimized</h5>
 										<p>Afterburner2 has only one image, which is 3KB in size.</p>
 									</div>
-								</div></div>
+								</div>
 							</div>
 						</div>
 					</section>
@@ -173,10 +172,10 @@
 						<h2 class="title">Demo Information</h2>
 						<p class="rt-border-bottom largepaddingbottom">All demo content is for <strong>sample</strong> purposes only, intended to a live site. Any content images are the copyright of RocketTheme, or their respective rights holders, and cannot be used without prior permission. A <strong>RocketLauncher</strong> is available, which allows you to mimic this demo.</p>
 
-						<span class="rt-grey rt-mini-icon icon-facebook"></span>
-						<span class="rt-grey rt-mini-icon icon-twitter"></span>
-						<span class="rt-grey rt-mini-icon icon-google-plus"></span>
-						<span class="rt-grey rt-mini-icon icon-rss"></span>
+						<span class="rt-grey rt-mini-icon fa fa-facebook"></span>
+						<span class="rt-grey rt-mini-icon fa fa-twitter"></span>
+						<span class="rt-grey rt-mini-icon fa fa-google-plus"></span>
+						<span class="rt-grey rt-mini-icon fa fa-rss"></span>
 					</div>
 				</div>
 				<div class="block size-1-3 pure-1-3">
