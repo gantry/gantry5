a10cca808e8314104b5d38c98aceec69acc4225e
Kevin DuCommun <kevin@rockettheme.com>
2014-04-15 11:00:26 -0500

    afterburner2 test template updates

--- templates/afterburner2/afterburner2.php
+++ templates/afterburner2/afterburner2.php
@@ -5,6 +5,7 @@
 	<title>Gantry Playground</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="../../css-compiled/nucleus.css" type="text/css" />
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.css" type="text/css" />
 	<link rel="stylesheet" href="css-compiled/afterburner2.css" type="text/css" />
 	<!--[if IE 9]>
 		<link rel="stylesheet" href="css/nucleus-ie9.css" type="text/css" />
@@ -35,56 +36,56 @@
 				<div class="grid pure-g-r">
 					<div class="block pure-u-1">
 						<div class="content">
-							<div class="custom promo nomargintop nopaddingtop nomarginbottom nopaddingbottom">
-								<div class="rt-center largemargintop largemarginbottom smallpaddingbottom">
-									<h1 class="largemargintop largemarginbottom medpaddingbottom">Fast, Responsive &amp; Free!</h1>
+							<div class="promo">
+								<div class="rt-center">
+									<h1>Fast, Responsive &amp; Free!</h1>
 									<h4 class="nobold"><em>Afterburner2 is a streamlined Gantry template, focused on speed and performance.</em></h4>
-								</div>	    
-								<div class="grid pure-g-r">
-									<div class="block pure-u-1-3">
-									    <div class="gantry-width-spacer">
-											<div class="gantry-width-30 gantry-width-block">        
-												<div class="largemarginright">
-													<div class="rt-num">1.0</div>
-												</div>				
-										 	</div>   
-											<div class="gantry-width-70 gantry-width-block">
-									        	<h5>Gantry 4 Core</h5>
-												<span>Built on the Gantry 4 Framework, Afterburner2 benefits from its rich<span class="visible-large">, powerful</span> core.</span>
-												<br><a href="http://www.gantry-framework.org" class="readon largemargintop"><span>Read More</span></a>
-											</div>
-									    </div>
-									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="grid pure-g-r">
+					<div class="block pure-u-1-3">
+						<div class="content">
+							<div class="grid pure-g-r">
+								<div class="block size-1-3 pure-u-1-3">     
+									<div class="rt-num">1.0</div>		
+							 	</div>   
+								<div class="block pure-u-2-3">
+						        	<h5>Gantry 4 Core</h5>
+									<p>Built on the Gantry 4 Framework, Afterburner2 benefits from its rich, powerful core.</p>
+									<a href="#" class="readon">Read More</a>
+								</div>
+							</div>
+						</div>
+					</div>
 
-									<div class="block pure-u-1-3">
-									    <div class="gantry-width-spacer">
-											<div class="gantry-width-30 gantry-width-block">        
-												<div class="largemarginright">
-													<div class="rt-num">2.0</div>
-												</div>				
-										 	</div>   
-											<div class="gantry-width-70 gantry-width-block">
-									        	<h5>Performance</h5>  
-												<span>An optimized amount of features and extras are used in this template to focus on speed.</span>
-												<br><a href="#" class="readon largemargintop"><span>Read More</span></a>
-											</div>
-									    </div>
-									</div>
+					<div class="block pure-u-1-3">
+						<div class="content">
+							<div class="grid pure-g-r">
+								<div class="block size-1-3 pure-u-1-3">        
+									<div class="rt-num">2.0</div>
+								</div>	
+								<div class="block pure-u-2-3">
+						        	<h5>Performance</h5>  
+									<p>An optimized amount of features and extras are used in this template to focus on speed.</p>
+									<a href="#" class="readon">Read More</a>
+								</div>
+							</div>
+						</div>
+					</div>
 
-									<div class="block pure-u-1-3">
-									    <div class="gantry-width-spacer">
-											<div class="gantry-width-30 gantry-width-block">        
-												<div class="largemarginright">
-													<div class="rt-num">3.0</div>
-												</div>				
-										 	</div>   
-											<div class="gantry-width-70 gantry-width-block">
-									        	<h5>RokBooster<span class="hidden-tablet"> Ready</span></h5>  
-												<span>Compress CSS/scripts/images <span class="hidden-tablet">with RokBooster </span>to increase speed even further.</span>
-												<br><a href="http://www.rockettheme.com/extensions-joomla/rokbooster" class="readon largemargintop"><span>Read More</span></a>
-											</div>
-									    </div>
-									</div>
+					<div class="block pure-u-1-3">
+						<div class="content">
+							<div class="grid pure-g-r">
+								<div class="block size-1-3 pure-u-1-3">       
+									<div class="rt-num">3.0</div>		
+							 	</div>   
+								<div class="block pure-u-2-3">
+						        	<h5>RokBooster<span class="hidden-tablet"> Ready</span></h5>  
+									<p>Compress CSS/scripts/images <span class="hidden-tablet">with RokBooster </span>to increase speed even further.</p>
+									<a href="#" class="readon">Read More</a>
 								</div>
 							</div>
 						</div>
@@ -99,14 +100,65 @@
 				<div class="block pure-u-2-3">
 					<section id="main">
 						<div class="content">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies tristique nulla et mattis. Phasellus id massa eget nisl congue blandit sit amet id ligula. Praesent et nulla eu augue tempus sagittis. Mauris faucibus nibh et nibh cursus in vestibulum sapien egestas. Curabitur ut lectus tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							<h2 class="title">Popular Features</h3>
+							<div class="rt-relative largemarginbottom  largepaddingbottom largepaddingright rt-border-right rt-border-bottom">
+								<div class="rt-big-icon rt-blue fa fa-cogs"></div>
+								<h5 class="smallmarginbottom nomargintop">Framework</h5>
+								<em><small>Advanced Features</small></em>
+								<p>Gantry provides the template foundation, with its standard set of features, such as the advanced template manager.</p>
+								<p><a href="http://gantry-framework.org/" class="readon">Read More</a></p>
+							</div>
+
+							<h2 class="title">Template Extras</h3>
+							<div class="grid pure-g-r">
+								<div class="block pure-u-1-2"><div class="gantry-width-spacer">
+									<div class="rt-relative largemarginbottom largepaddingright rt-border-right rt-border-bottom">
+										<p>Afterburner2 is an experiment in how streamlined a Gantry template can be whilst still being stunning and usable. Many extras have been removed, but still contains styling for major elements.</p>
+									</div>
+								</div></div>
+
+								<div class="block pure-u-1-2"><div class="gantry-width-spacer">
+									<div class="rt-relative largemarginbottom largepaddingright rt-border-right rt-border-bottom">
+										<div class="rt-small-icon rt-red fa fa-tasks"></div>
+										<h5 class="smallmarginbottom nomargintop">Dropdowns</h5>
+										<p>A CSS based dropdown menu system.</p>
+									</div>
+								</div></div>
+							</div>
+							<div class="grid pure-g-r">
+								<div class="block pure-u-1-2"><div class="gantry-width-spacer">
+									<div class="rt-relative largemarginbottom largepaddingright rt-border-right">
+										<div class="rt-small-icon rt-grey fa fa-list"></div>
+										<h5 class="smallmarginbottom nomargintop">Typography</h5>
+										<p>Elements are styled by a streamlined version of Bootstrap.</p>
+									</div>
+								</div></div>
+
+								<div class="block pure-u-1-2"><div class="gantry-width-spacer">
+									<div class="rt-relative largemarginbottom largepaddingright rt-border-right">
+										<div class="rt-small-icon rt-blue fa fa-edit"></div>
+										<h5 class="smallmarginbottom nomargintop">Optimized</h5>
+										<p>Afterburner2 has only one image, which is 3KB in size.</p>
+									</div>
+								</div></div>
+							</div>
 						</div>
 					</section>
 				</div>
 				<div class="block size-1-3 pure-1-3">
 					<section id="sidebar">
 						<div class="content">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies tristique nulla et mattis. Phasellus id massa eget nisl congue blandit sit amet id ligula. Praesent et nulla eu augue tempus sagittis. Mauris faucibus nibh et nibh cursus in vestibulum sapien egestas. Curabitur ut lectus tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							<h2 class="title">Preset Styles</h2>
+							<p>Afterburner2 is outfitted with 3 preset style variations. Each have configurable options for header, footer and menu style.</p>
+
+							<p>Further, you can change the accent and link colors via an interactive color picker interface in the template manager.</p>
+
+							<p><a class="readon" href="/live/joomla/afterburner2/presets">Read More</a></p>
+						</div>
+						<div class="content box1">
+							<h2 class="title">Module Variations</h2>
+							<p>Several module class suffixes come with the template. box1 is the singular style suffix, which is applied to the current module. Others are structural, that allow you to change the margins/paddings of the module.</p>
+							<a href="/live/joomla/afterburner2/features/module-variations" class="readon"><span>Read More</span></a>
 						</div>
 					</section>
 				</div>
@@ -118,12 +170,44 @@
 			<div class="grid pure-g-r">
 				<div class="block pure-u-2-3">
 					<div class="content">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies tristique nulla et mattis. Phasellus id massa eget nisl congue blandit sit amet id ligula. Praesent et nulla eu augue tempus sagittis. Mauris faucibus nibh et nibh cursus in vestibulum sapien egestas. Curabitur ut lectus tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						<h2 class="title">Demo Information</h2>
+						<p class="rt-border-bottom largepaddingbottom">All demo content is for <strong>sample</strong> purposes only, intended to a live site. Any content images are the copyright of RocketTheme, or their respective rights holders, and cannot be used without prior permission. A <strong>RocketLauncher</strong> is available, which allows you to mimic this demo.</p>
+
+						<span class="rt-grey rt-mini-icon icon-facebook"></span>
+						<span class="rt-grey rt-mini-icon icon-twitter"></span>
+						<span class="rt-grey rt-mini-icon icon-google-plus"></span>
+						<span class="rt-grey rt-mini-icon icon-rss"></span>
 					</div>
 				</div>
 				<div class="block size-1-3 pure-1-3">
 					<div class="content">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies tristique nulla et mattis. Phasellus id massa eget nisl congue blandit sit amet id ligula. Praesent et nulla eu augue tempus sagittis. Mauris faucibus nibh et nibh cursus in vestibulum sapien egestas. Curabitur ut lectus tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						<h2 class="title">Contact Us</h2>
+						<div class="grid pure-g-r">
+							<div class="block size-1-7 pure-u-1-7">
+								<h4><span class="fa fa-phone"></span></h4>	
+							</div>
+							<div class="block pure-u-6-7">
+								<strong>+1 (555) 555-555-5555</strong>
+								<br><span>+1 (555) 555-555-5556</span>
+							</div>
+						</div>
+						<div class="grid pure-g-r">
+							<div class="block size-1-7 pure-u-1-7">
+								<h4><span class="fa fa-home"></span></h4>	
+							</div>
+							<div class="block pure-u-6-7">
+								<span>123 Joomla! Boulevard</span>
+								<br><span>Seattle, WA 00000, USA</span> 
+							</div>
+						</div>
+						<div class="grid pure-g-r">
+							<div class="block size-1-7 pure-u-1-7">
+								<h4 class="nomargintop"><span class="fa fa-envelope"></span></h4>	
+							</div>
+							<div class="block pure-u-6-7">
+								<a href="mailto:noreply@domain.com">noreply@domain.com</a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -134,7 +218,9 @@
 			<div class="grid pure-g-r">
 					<div class="block pure-u-1">
 						<div class="content">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							<div class="rt-copyright-block">
+								<a href="#">Designed by RocketTheme</a>
+							</div>
 						</div>
 					</div>
 				</div>
