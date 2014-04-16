c3489ad0e70f55df6b7413f5361d81b6265ce45d
Kevin DuCommun <kevin@rockettheme.com>
2014-04-16 11:55:35 -0500

    added html5 shim for html5 element support in ie8 and 9

--- core/head.php
+++ core/head.php
@@ -4,7 +4,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css-compiled/nucleus.css" type="text/css" />
 	<link rel="stylesheet" href="css-compiled/template.css" type="text/css" />
-	<!--[if IE 9]>
+	<!--[if (gte IE 8)&(lte IE 9)]>
+		<script type="text/javascript" src="js/html5shiv-printshiv.min.js"></script>
 		<link rel="stylesheet" href="css/nucleus-ie9.css" type="text/css" />
 		<link rel="stylesheet" href="css/pure/grids-min.css" type="text/css" />
 	<![endif]-->
