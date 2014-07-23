709eb392a4c810597c0085d6026d4ddcd9677b56
Matias Griese <matias@kunena.org>
2014-07-23 12:12:58 +0300

    Add timber starter theme

---
+++ wordpress/author.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying Author Archive pages
+ *
+ * Methods for TimberHelper can be found in the /functions sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+global $wp_query;
+
+$data = Timber::get_context();
+$data['posts'] = Timber::get_posts();
+if (isset($query_vars['author'])){
+	$author = new TimberUser($wp_query->query_vars['author']);
+	$data['author'] = $author;
+	$data['title'] = 'Author Archives: ' . $author->name();
+}
+Timber::render(array('author.twig', 'archive.twig'), $data);
---
+++ wordpress/sidebar.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ */
+
+Timber::render(array('sidebar.twig'), $data);
