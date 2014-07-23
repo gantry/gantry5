050157e7020cd67f6d360dd6d74603207f7e9464
Jakub Baran <jakub@rockettheme.com>
2014-07-23 14:24:21 +0200

    some refactoring

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -1,48 +1,48 @@
 <?php
 
-if (!class_exists('Timber')){
-	add_action( 'admin_notices', function(){
-		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . admin_url('plugins.php#timber') . '">' . admin_url('plugins.php') . '</a></p></div>';
-	});
+if ( !class_exists( 'Timber' ) ) {
+	add_action( 'admin_notices', function() {
+		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . admin_url( 'plugins.php#timber' ) . '">' . admin_url( 'plugins.php' ) . '</a></p></div>';
+	} );
 	return;
 }
 
 class GantrySite extends TimberSite {
 
-	function __construct(){
-		add_theme_support('post-formats');
-		add_theme_support('post-thumbnails');
-		add_theme_support('menus');
-		add_filter('timber_context', array($this, 'add_to_context'));
-		add_filter('get_twig', array($this, 'add_to_twig'));
-		add_action('init', array($this, 'register_post_types'));
-		add_action('init', array($this, 'register_taxonomies'));
+	function __construct() {
+		add_theme_support( 'post-formats' );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'menus' );
+		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+		add_action( 'init', array( $this, 'register_post_types' ) );
+		add_action( 'init', array( $this, 'register_taxonomies' ) );
 		parent::__construct();
 	}
 
-	function register_post_types(){
+	function register_post_types() {
 		//this is where you can register custom post types
 	}
 
-	function register_taxonomies(){
+	function register_taxonomies() {
 		//this is where you can register custom taxonomies
 	}
 
-	function add_to_context($context){
+	function add_to_context( $context ) {
 		$context['menu'] = new TimberMenu();
 		$context['site'] = $this;
 
 		// Include Gantry specific things to the context.
-		$context['pageLayout'] = json_decode(file_get_contents('test/nucleus.json'));
+		$context['pageLayout'] = json_decode( file_get_contents( 'test/nucleus.json' ) );
 		return $context;
 	}
 
-	function add_to_twig($twig){
+	function add_to_twig( $twig ) {
 		/* this is where you can add your own fuctions to twig */
 		$loader = $twig->getLoader();
-		$loader->addPath(__DIR__ . '/nucleus', 'nucleus');
-		$twig->addExtension(new Twig_Extension_StringLoader());
-		$twig->addFilter('myfoo', new Twig_Filter_Function('myfoo'));
+		$loader->addPath( __DIR__ . '/nucleus', 'nucleus' );
+		$twig->addExtension( new Twig_Extension_StringLoader() );
+		$twig->addFilter(' myfoo', new Twig_Filter_Function( 'myfoo' ) );
 		return $twig;
 	}
 
@@ -50,7 +50,7 @@ class GantrySite extends TimberSite {
 
 new GantrySite();
 
-function myfoo($text){
+function myfoo( $text ) {
     $text .= ' bar!';
     return $text;
 }
--- wordpress/single.php
+++ wordpress/single.php
@@ -11,14 +11,13 @@
 
 $context = Timber::get_context();
 $post = Timber::query_post();
+
 $context['post'] = $post;
 $context['wp_title'] .= ' - ' . $post->title();
 $context['comment_form'] = TimberHelper::get_comment_form();
 
-if (post_password_required($post->ID)){
-	Timber::render('single-password.twig', $context);
+if ( post_password_required( $post->ID ) ) {
+	Timber::render( 'single-password.twig', $context );
 } else {
-	Timber::render(array('single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig'), $context);
+	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
 }
-
-
