71c6d34706eaf86ec9256557b6d3ca8e239ecf55
Matias Griese <matias@kunena.org>
2014-08-28 20:39:44 +0300

    Fix name conflict in Grav Theme

--- src/classes/Gantry/Platform/Grav/Framework/Theme.php
+++ src/classes/Gantry/Platform/Grav/Framework/Theme.php
@@ -4,14 +4,14 @@ namespace Gantry\Framework;
 use Gantry\Framework\Base\ThemeTrait as GantryThemeTrait;
 use Grav\Common\Theme as BaseTheme;
 use Grav\Common\Grav;
-use Grav\Common\Config;
+use Grav\Common\Config as GravConfig;
 use RocketTheme\Toolbox\File\YamlFile;
 
 class Theme extends BaseTheme
 {
     use GantryThemeTrait;
 
-    public function __construct(Grav $grav, Config $config, $name)
+    public function __construct(Grav $grav, GravConfig $config, $name)
     {
         parent::__construct($grav, $config, $name);
 
