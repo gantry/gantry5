47b6a147dada8decff5f166a4856d085c49a4f63
Matias Griese <matias@kunena.org>
2014-10-28 13:33:58 +0200

    Optimize autoloading

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,26 +1,33 @@
 <?php
-
-require_once __DIR__ . '/includes/defines.php';
-
-$errorMessage = 'You are running PHP %s, but Gantry Framework needs at least PHP %s to run.';
-
-// Fail safe version check for PHP <5.4.0, which do not support Bootstrap.
-if (version_compare($ver = PHP_VERSION, '5.4.0', '<')) {
-    throw new \RuntimeException(sprintf($errorMessage, $ver, GANTRY5_MIN_PHP));
-}
-
-$loader = require_once __DIR__ . '/includes/autoload.php';
+namespace Gantry;
 
 use \Tracy\Debugger;
 
-// If debug mode is enabled, enable tracy in development mode.
-if (DEBUG && !Debugger::isEnabled()) {
-    Debugger::enable(Debugger::DEVELOPMENT);
+function bootstrap()
+{
+    $errorMessage = 'You are running PHP %s, but Gantry Framework needs at least PHP %s to run.';
+
+    // Fail safe version check for PHP <5.4.0, which do not support Bootstrap.
+    if (version_compare($phpVersion = PHP_VERSION, '5.4.0', '<')) {
+        throw new \RuntimeException(sprintf($errorMessage, $phpVersion, GANTRY5_MIN_PHP));
+    }
+
+    require_once __DIR__ . '/includes/defines.php';
+    require_once __DIR__ . '/includes/autoload.php';
+
+    $loader = autoload();
+
+    // If debug mode is enabled, enable tracy in development mode.
+    if (DEBUG && !Debugger::isEnabled()) {
+        Debugger::enable(Debugger::DEVELOPMENT);
+    }
+
+    // Allow nice debug messages by using tracy.
+    if (version_compare($phpVersion, GANTRY5_MIN_PHP, '<')) {
+        throw new \RuntimeException(sprintf($errorMessage, $phpVersion, GANTRY5_MIN_PHP));
+    }
+
+    return $loader;
 }
 
-// Allow nice debug messages by using tracy.
-if (version_compare($ver, GANTRY5_MIN_PHP, '<')) {
-    throw new \RuntimeException(sprintf($errorMessage, $ver, GANTRY5_MIN_PHP));
-}
-
-return $loader;
+return bootstrap();
