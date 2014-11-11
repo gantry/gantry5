e58185054c90e4c15fb8e8c9549730ea33f95cc9
Djamil Legato <djamil@djamil.it>
2014-11-11 15:18:46 -0800

    Initial logic for reading layout presets Fixed fontawesome references

--- src/classes/Gantry/Admin/Controller/Json/Layouts.php
+++ src/classes/Gantry/Admin/Controller/Json/Layouts.php
@@ -2,12 +2,30 @@
 namespace Gantry\Admin\Controller\Json;
 
 use Gantry\Component\Controller\JsonController;
+use Gantry\Component\Filesystem\Folder;
 use Gantry\Component\Response\JsonResponse;
+use RocketTheme\Toolbox\File\JsonFile;
 
 class Layouts extends JsonController
 {
     public function index(array $params)
     {
-        return new JsonResponse(['foo' => 1]);
+        $options = [
+            'compare' => 'Filename',
+            'pattern' => '|\.json|',
+            'filters' => ['key' => '|\.json|'],
+            'key' => 'SubPathname',
+            'value' => 'Pathname'
+        ];
+
+        // FIXME: need access to current theme stream 'theme://common/layouts'
+        $files = Folder::all(realpath(JPATH_THEMES . '/../../templates/gantry/common/layouts'), $options);
+
+        $response = [];
+        foreach($files as $structure) {
+            $content = JsonFile::instance($structure)->content();
+            $response[$content['name']] = $content;
+        }
+        return new JsonResponse($response);
     }
 }
