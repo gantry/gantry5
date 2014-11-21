9a1912d460ad155e28394abda93c6148080550f2
Matias Griese <matias@kunena.org>
2014-11-21 15:33:34 +0200

    Joomla: Update afterburner streams

--- themes/afterburner2/joomla/includes/template.php
+++ themes/afterburner2/joomla/includes/template.php
@@ -4,6 +4,17 @@ defined('_JEXEC') or die;
 // Define the template.
 class AfterburnerTemplate extends \Gantry\Framework\Theme {}
 
+// Initialize theme stream.
+$gantry['platform']->set(
+    'streams.theme.prefixes',
+    ['' => [
+        "themes://{$gantry['theme.name']}",
+        "themes://{$gantry['theme.name']}/common",
+        "themes://gantry",
+        "themes://gantry/common"]
+    ]
+);
+
 // Define Gantry services.
 $gantry['theme'] = function ($c)  {
     return new AfterburnerTemplate($c['theme.path'], $c['theme.name']);
