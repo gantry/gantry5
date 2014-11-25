29f87da9e3a08273834521d38df204c977a0ed52
Matias Griese <matias@kunena.org>
2014-11-25 20:04:19 +0200

    Fix WordPress admin

---
+++ src/classes/Gantry/Platform/WordPress/Admin/Theme/Theme.php
@@ -0,0 +1,89 @@
+<?php
+namespace Gantry\Admin\Theme;
+
+use Gantry\Component\Filesystem\Folder;
+use Gantry\Component\Filesystem\Streams;
+use Gantry\Component\Twig\TwigExtension;
+use Gantry\Framework\Base\Theme as BaseTheme;
+use RocketTheme\Toolbox\ResourceLocator\UniformResourceLocator;
+
+class Theme extends BaseTheme
+{
+    public $path;
+
+    public function __construct( $path, $name = '' )
+    {
+        parent::__construct($path, $name);
+
+        $this->boot();
+    }
+
+    protected function boot()
+    {
+        $gantry = \Gantry\Framework\Gantry::instance();
+
+        $relpath = Folder::getRelativePath($this->path);
+
+        /** @var Streams $streams */
+        $streams = $gantry['streams'];
+        $streams->add(['gantry-admin' => [
+                'prefixes' => [
+                    '' => ['gantry-theme://admin', $relpath, $relpath . '/common'],
+                    'assets/' => ['gantry-theme://admin', $relpath, $relpath . '/common']
+                ]
+            ]
+            ]);
+        $streams->register();
+    }
+
+    public function render($file, array $context = array())
+    {
+        $gantry = \Gantry\Framework\Gantry::instance();
+
+        /** @var UniformResourceLocator $locator */
+        $locator = $gantry['locator'];
+
+        $loader = new \Twig_Loader_Filesystem($locator->findResources('gantry-admin://templates'));
+
+        $params = array(
+            'cache' => $locator->findResource('gantry-cache://') . '/twig',
+            'debug' => true,
+            'auto_reload' => true,
+            'autoescape' => false
+        );
+
+        $twig = new \Twig_Environment($loader, $params);
+
+        $this->add_to_twig($twig);
+
+        // Include Gantry specific things to the context.
+        $context = $this->add_to_context($context);
+
+        return $twig->render($file, $context);
+    }
+
+    public function add_to_twig(\Twig_Environment $twig, \Twig_Loader_Filesystem $loader = null)
+    {
+        $gantry = \Gantry\Framework\Gantry::instance();
+
+        /** @var UniformResourceLocator $locator */
+        $locator = $gantry['locator'];
+
+        if (!$loader) {
+            $loader = $twig->getLoader();
+        }
+        $loader->setPaths($locator->findResources('gantry-admin://templates'), 'gantry-admin');
+
+        $twig->addExtension(new TwigExtension);
+        return $twig;
+    }
+
+    public function add_to_context( array $context )
+    {
+        $context = parent::add_to_context( $context );
+
+        $this->url = $context['site']->theme->link;
+
+        return $context;
+    }
+}
