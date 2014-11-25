386ea2b5c7e308acf470e9716b8d52984e8444d4
Matias Griese <matias@kunena.org>
2014-11-25 13:18:04 +0200

    Update standalone themes

---
+++ themes/afterburner2/standalone/includes/theme.php
@@ -0,0 +1,20 @@
+<?php
+class_exists('\\Gantry\\Framework\\Gantry') or die;
+
+// Define the template.
+class GantryTheme extends \Gantry\Framework\Theme {}
+
+// Initialize theme stream.
+$gantry['platform']->set(
+    'streams.theme.prefixes',
+    ['' => [
+        "themes://{$gantry['theme.name']}", "themes://{$gantry['theme.name']}/common",
+        "themes://gantry", "themes://gantry/common"
+        ]
+    ]
+);
+
+// Define Gantry services.
+$gantry['theme'] = function($c)  {
+    return new GantryTheme($c['theme.path'], $c['theme.name']);
+};
