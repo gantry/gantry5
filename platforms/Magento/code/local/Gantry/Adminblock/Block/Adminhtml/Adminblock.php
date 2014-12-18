d0ffeeda3008a89299ed478b04aeaa59ef0db6ae
Samuel Mahoney <info@divinemonkey.co.uk>
2014-12-18 13:12:31 +0000

    Adding Magento module for loading Gantry admin

---
+++ platforms/Magento/code/local/Gantry/Adminblock/Block/Adminhtml/Adminblock.php
@@ -0,0 +1,10 @@
+<?php
+
+class Gantry_Adminblock_Block_Adminhtml_Adminblock extends Mage_Core_Block_Template {
+
+	protected function loadGantry(){
+		$gantry = "GANTRY LOADED";
+		return $gantry;
+	}
+
+}
