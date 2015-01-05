cdee4041ba79c02bccc226a993136ba30353545e
Matias Griese <matias@kunena.org>
2015-01-05 22:31:13 +0200

    Change pages/create URL and include layout to html

--- src/classes/Gantry/Admin/Controller/Html/Pages.php
+++ src/classes/Gantry/Admin/Controller/Html/Pages.php
@@ -6,13 +6,40 @@ use RocketTheme\Toolbox\File\JsonFile;
 
 class Pages extends HtmlController
 {
+    protected $httpVerbs = [
+        'GET' => [
+            '/'         => 'index',
+            '/create'   => 'create',
+            '/create/*' => 'create',
+            '/*'        => 'display',
+            '/*/edit'   => 'edit'
+        ],
+        'POST' => [
+            '/'  => 'store'
+        ],
+        'PUT' => [
+            '/*' => 'replace'
+        ],
+        'PATCH' => [
+            '/*' => 'update'
+        ],
+        'DELETE' => [
+            '/*' => 'destroy'
+        ]
+    ];
+
     public function index()
     {
         return $this->container['admin.theme']->render('@gantry-admin/pages_index.html.twig', $this->params);
     }
 
-    public function create()
+    public function create($id = null)
     {
+        $locator = $this->container['locator'];
+
+        // TODO: remove hardcoded layout.
+        $this->params['layout'] = JsonFile::instance($locator('gantry-theme://layouts/presets/'.$id.'.json'))->content();
+
         return $this->container['admin.theme']->render('@gantry-admin/pages_create.html.twig', $this->params);
     }
 
--- src/classes/Gantry/Framework/Base/Gantry.php
+++ src/classes/Gantry/Framework/Base/Gantry.php
@@ -30,6 +30,8 @@ class Gantry extends Container
         $routes = $this->offsetGet('routes');
         $route = isset($routes[$path]) ? $routes[$path] : $routes[1];
 
+        $path = implode('/', func_get_args());
+
         return '/' . ltrim($this->offsetGet('base_url') . sprintf($route, $path), '/');
     }
 
