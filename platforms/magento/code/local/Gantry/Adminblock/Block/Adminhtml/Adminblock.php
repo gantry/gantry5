f4fe495add5752061387501c8913aafa5f2d7184
Matias Griese <matias@kunena.org>
2015-01-15 10:04:28 +0200

    Admin: Fix exception in Magento

--- platforms/magento/code/local/Gantry/Adminblock/Block/Adminhtml/Adminblock.php
+++ platforms/magento/code/local/Gantry/Adminblock/Block/Adminhtml/Adminblock.php
@@ -13,6 +13,9 @@ class Gantry_Adminblock_Block_Adminhtml_Adminblock extends Mage_Core_Block_Templ
             define('GANTRYADMIN_PATH', GANTRY5_ROOT . '/app/code/local/Gantry');
         }
 
+        // FIXME: add base url for Gantry admin.
+        $gantry['base_url'] =  '/';
+
         $gantry['router'] = function ($c)
         {
             return new \Gantry\Admin\Router($c);
