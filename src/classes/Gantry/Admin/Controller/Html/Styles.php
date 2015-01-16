be0fe6bb47563d1d1585886b1ad7e5ce8d684074
coder4life <coder4life.dev@gmail.com>
2015-01-16 12:17:32 -0800

    Adding styles with some basic class control
    Not complete...
    
    Very similar to Particles implementation, in some cases classes are copied but will be changed to reflect some structual changes.

--- src/classes/Gantry/Admin/Controller/Html/Styles.php
+++ src/classes/Gantry/Admin/Controller/Html/Styles.php
@@ -1,12 +1,125 @@
 <?php
 namespace Gantry\Admin\Controller\Html;
 
+use Gantry\Component\Config\Blueprints;
+use Gantry\Component\Config\CompiledBlueprints;
+use Gantry\Component\Config\ConfigFileFinder;
 use Gantry\Component\Controller\HtmlController;
+use Gantry\Component\File\CompiledYamlFile;
+use Gantry\Component\Filesystem\Folder;
+use Gantry\Framework\Base\Gantry;
+use RocketTheme\Toolbox\ResourceLocator\UniformResourceLocator;
 
 class Styles extends HtmlController
 {
+
+    protected $httpVerbs = [
+        'GET' => [
+            '/'              => 'index',
+            '/styles'        => 'undefined',
+            '/styles/*'      => 'display',
+            '/styles/*/**'   => 'formfield',
+        ],
+        'POST' => [
+            '/'         => 'forbidden',
+            '/styles'   => 'forbidden',
+            '/styles/*' => 'save'
+        ],
+        'PUT' => [
+            '/'         => 'forbidden',
+            '/styles'   => 'forbidden',
+            '/styles/*' => 'save'
+        ],
+        'PATCH' => [
+            '/'         => 'forbidden',
+            '/styles'   => 'forbidden',
+            '/styles/*' => 'save'
+        ],
+        'DELETE' => [
+            '/'            => 'forbidden',
+            '/styles'   => 'forbidden',
+            '/styles/*' => 'reset'
+        ]
+    ];
+
     public function index()
     {
+        $this->params['styles'] = $this->container['styles']->group();
+
         return $this->container['admin.theme']->render('@gantry-admin/styles.html.twig', $this->params);
+    }
+
+    public function display($id)
+    {
+        $style = $this->container['styles']->get($id);
+        $blueprints = new Blueprints($style);
+        $prefix = 'styles.' . $id;
+
+        $this->params += [
+            'style' => $blueprints,
+            'data' =>  Gantry::instance()['config']->get($prefix),
+            'id' => $id,
+            'parent' => 'settings',
+            'route' => 'settings.' . $prefix,
+            'skip' => ['enabled']
+        ];
+
+        return $this->container['admin.theme']->render('@gantry-admin/styles/item.html.twig', $this->params);
+    }
+
+    public function formfield($id)
+    {
+        $path = func_get_args();
+
+        $style = $this->container['styles']->get($id);
+
+        // Load blueprints.
+        $blueprints = new Blueprints($style);
+
+        list($fields, $path, $value) = $blueprints->resolve(array_slice($path, 1), '/');
+
+        if (!$fields) {
+            throw new \RuntimeException('Page Not Found', 404);
+        }
+
+        // Get the prefix.
+        $prefix = "styles.{$id}." . implode('.', $path);
+        if ($value !== null) {
+            $parent = $fields;
+            $fields = ['fields' => $fields['fields']];
+            $prefix .= '.' . $value;
+        }
+        array_pop($path);
+
+        $this->params = [
+                'blueprints' => $fields,
+                'data' =>  $this->container['config']->get($prefix),
+                'parent' => $path ? "settings/styles/{$id}/" . implode('/', $path) : "settings/styles/{$id}",
+                'route' => 'settings.' . $prefix
+            ] + $this->params;
+
+        if (isset($parent['key'])) {
+            $this->params['key'] = $parent['key'];
+        }
+
+        return $this->container['admin.theme']->render('@gantry-admin/settings/field.html.twig', $this->params);
+    }
+
+    public function save($id)
+    {
+        $this->params += [
+            'data' => $_POST,
+        ];
+
+        return $this->display($id);
+    }
+
+    public function reset($id)
+    {
+        $this->params += [
+            'data' => [],
+        ];
+
+        return $this->display($id);
     }
 }
---
+++ src/classes/Gantry/Admin/Styles/Styles.php
@@ -0,0 +1,82 @@
+<?php
+namespace Gantry\Admin\Particles;
+
+use Gantry\Component\Config\Blueprints;
+use Gantry\Component\Config\CompiledBlueprints;
+use Gantry\Component\Config\ConfigFileFinder;
+use Gantry\Component\File\CompiledYamlFile;
+use Gantry\Component\Filesystem\Folder;
+use Gantry\Framework\Base\Gantry;
+use RocketTheme\Toolbox\ResourceLocator\UniformResourceLocator;
+
+class Styles
+{
+    protected $container;
+    protected $files;
+    protected $styles;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    public function all()
+    {
+        if (!$this->styles)
+        {
+            $files = $this->locateStyles();
+
+            $this->styles = [];
+            foreach ($files as $key => $file) {
+                $filename = key($file);
+                $this->styles[$key] = CompiledYamlFile::instance(GANTRY5_ROOT . '/' . $filename)->content();
+            }
+        }
+
+        return $this->styles;
+    }
+
+    public function group()
+    {
+        $styles = $this->all();
+
+        $list = [];
+        foreach ($styles as $name => $style) {
+            $type = isset($style['type']) ? $style['type'] : 'style';
+            $list[$type][$name] = $style;
+        }
+
+        return $list;
+    }
+
+    public function get($id)
+    {
+        if ($this->styles[$id]) {
+            return $this->styles[$id];
+        }
+
+        $files = $this->locateStyles();
+
+        if (empty($files[$id])) {
+            throw new \RuntimeException("Settings for '{$id}' not found.", 404);
+        }
+
+        $filename = key($files[$id]);
+        $particle = CompiledYamlFile::instance(GANTRY5_ROOT . '/' . $filename)->content();
+
+        return $particle;
+    }
+
+    protected function locateStyles()
+    {
+        if (!$this->files) {
+            /** @var UniformResourceLocator $locator */
+            $locator = $this->container['locator'];
+            $paths = $locator->findResources('gantry-styles://');
+
+            $this->files = (new ConfigFileFinder)->listFiles($paths);
+        }
+
+        return $this->files;
+    }
+}
