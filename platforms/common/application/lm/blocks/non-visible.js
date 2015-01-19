fc981feec726e692f9deabe161fdf27ea6d0794c
Djamil Legato <djamil@djamil.it>
2015-01-19 12:03:32 -0800

    Updated layout presets with non-visible section

--- platforms/common/application/lm/blocks/non-visible.js
+++ platforms/common/application/lm/blocks/non-visible.js
@@ -15,6 +15,10 @@ var NonVisible = new prime({
         return '<div class="non-visible-section" data-lm-id="' + this.getId() + '" data-lm-blocktype="' + this.getType() + '"><div class="section-header clearfix"><h4 class="float-left">' + (this.getAttribute('name')) + '</h4></div></div>';
     },
 
+    getId: function() {
+        return this.id || (this.id = this.options.type);
+    },
+
     onDone: function(event) {
         if (!this.block.search('[data-lm-id]')) {
             this.grid.insert(this.block, 'bottom');
