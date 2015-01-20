5ff2f8335a390ebd4b341528468e74ab76b44ac3
Matias Griese <matias@kunena.org>
2015-01-20 21:31:16 +0200

    Fix 500 Internal Server Error on every response

--- src/classes/Gantry/Component/Response/Response.php
+++ src/classes/Gantry/Component/Response/Response.php
@@ -102,7 +102,7 @@ class Response
     {
         $code = $this->getStatusCode();
 
-        return $code . ' ' . isset($this->responseCodes[$code]) ? $this->responseCodes[$code] : 'Unknown error';
+        return $code . ' ' . (isset($this->responseCodes[$code]) ? $this->responseCodes[$code] : 'Unknown error');
     }
 
     /**
