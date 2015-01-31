d984b446f872ca85e32dd7644ca7e41d7aaaa1cf
Djamil Legato <djamil@djamil.it>
2015-01-31 00:12:02 -0800

    Allowing dropping items only between same levels

--- platforms/common/application/menu/menumanager.js
+++ platforms/common/application/menu/menumanager.js
@@ -53,7 +53,7 @@ var MenuManager = new prime({
             size = $(element).position();
 
         this.block = null;
-        this.type = element.parent('.g-main-nav') || element.matches('.g-main-nav') ? 'main' : 'columns'
+        this.type = element.parent('.g-main-nav') || element.matches('.g-main-nav') ? 'main' : 'columns';
 
         root.addClass('moving');
         var type = $(element).data('mm-id'),
@@ -86,10 +86,14 @@ var MenuManager = new prime({
         target = $(target);
         if (!this.placeholder) { this.placeholder = zen('li.block.placeholder[data-mm-placeholder]').style({ display: 'none' }); }
 
-        var position,
-            dataType = target.parent('.g-main-nav') || target.matches('.g-main-nav') ? 'main' : 'columns',
+        var targetType = target.parent('.g-main-nav') || target.matches('.g-main-nav') ? 'main' : 'columns',
             dataID = target.data('mm-id'),
-            originalID = this.block.data('mm-id');
+            dataLevel = target.data('mm-level'),
+            originalID = this.block.data('mm-id'),
+            originalLevel = this.block.data('mm-level');
+
+        // we only allow sorting between same level items
+        if (originalLevel !== dataLevel) { return; }
 
         // Check for adjacents and avoid inserting any placeholder since it would be the same position
         var exclude = ':not(.placeholder):not([data-mm-id="' + this.original.data('mm-id') + '"])',
@@ -100,56 +104,27 @@ var MenuManager = new prime({
 
         if (adjacents.before) { adjacents.before = $(adjacents.before[0]); }
         if (adjacents.after) { adjacents.after = $(adjacents.after[0]); }
-        if (dataType === 'main' && ((adjacents.before === target && location.x === 'after') || (adjacents.after === target && location.x === 'before'))) {
+        if (targetType === 'main' && ((adjacents.before === target && location.x === 'after') || (adjacents.after === target && location.x === 'before'))) {
             return;
         }
-        if (dataType === 'columns' && ((adjacents.before === target && location.y === 'below') || (adjacents.after === target && location.y === 'above'))) {
+        if (targetType === 'columns' && ((adjacents.before === target && location.y === 'below') || (adjacents.after === target && location.y === 'above'))) {
             return;
         }
 
         // handles the types cases and normalizes the locations (x and y)
-        var grid, block, method;
-
-        switch (dataType) {
+        switch (targetType) {
             case 'main':
-                /*var empty = !target.children(':not(.placeholder)');
-                // new particles cannot be dropped in existing grids, only empty ones
-                if (originalType !== 'grid' && !empty) { return; }
-
-                // grids cannot be dropped inside grids
-                if (originalType === 'grid' && empty) { return; }
-
-                // we are dropping a new particle into an empty grid, placeholder goes inside
-                if (empty) { this.placeholder.bottom(target); }
-                else {
-                    // we are sorting grids ordering, placeholder goes above/below
-                    method = (location.y === 'above' ? 'before' : 'after');
-                    this.placeholder[method](target);
-                }*/
-                /*if (!dataID) {
-                    target = target.find('> :last-child');
-                }*/
-
                 this.placeholder[location.x](target);
                 break;
             case 'columns':
-                method = (location.y === 'above' ? 'before' : 'after');
-                this.placeholder[method](target);
+                this.placeholder[location.y === 'above' ? 'before' : 'after'](target);
 
                 break;
         }
 
         // If it's not a block we don't want a small version of the placeholder
-        this.placeholder.removeClass('in-between').removeClass('in-between-grids').removeClass('in-between-grids-first').removeClass('in-between-grids-last');
-        this.placeholder.style({ display: 'block' })[dataType !== 'main' ? 'removeClass' : 'addClass']('in-between');
-        /*if (originalType === 'grid' && dataType === 'grid') {
-            var next = this.placeholder.nextSibling(),
-                previous = this.placeholder.previousSibling();
+        this.placeholder.style({ display: 'block' })[targetType !== 'main' ? 'removeClass' : 'addClass']('in-between');
 
-            this.placeholder.addClass('in-between-grids');
-            if (previous && !previous.data('lm-blocktype')) { this.placeholder.addClass('in-between-grids-first'); }
-            if (!next || !next.data('lm-blocktype')) { this.placeholder.addClass('in-between-grids-last'); }
-        }*/
     },
 
     nolocation: function(event) {
