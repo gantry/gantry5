b9e1684f8f99f8af521d78fed5ae01c441130914
Matias Griese <matias@kunena.org>
2015-02-03 20:28:48 +0200

    Remove themes/gantry/static, it will not be needed anymore as we have prime

--- themes/gantry/static/sections/bottom.php
+++
@@ -1,9 +0,0 @@
-<section id="bottom">
-	<div class="grid pure-g">
-		<div class="block pure-u-1-1">
-			<div class="content">
-				Mauris fermentum porta sem, non hendrerit enim bibendum consectetur. Fusce diam est, porttitor vehicula gravida at, accumsan bibendum tincidunt imperdiet. Maecenas quis magna faucibus, varius ante sit amet, varius augue. Praesent aliquam, augue ac pulvinar accumsan, risus arcu malesuada est, a imperdiet lacus libero ac tellus.
-			</div>
-		</div>
-	</div>
-</section>
