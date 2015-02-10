07bb02b34dc999ec655f911c0d9dc33a2e6b87a2
Djamil Legato <djamil@djamil.it>
2015-02-10 13:37:55 -0800

    Implemented SaveState class to keep track of changes after a save or after first load with Date stored for the records

---
+++ platforms/common/application/utils/save-state.js
@@ -0,0 +1,44 @@
+var prime      = require('prime'),
+    deepClone  = require('mout/lang/deepClone'),
+    objectDiff = require('objectdiff');
+
+var SaveState = new prime({
+
+    constructor: function(session) {
+        session = deepClone(session);
+        this.setSession(session);
+    },
+
+    setSession: function(session) {
+        session = !session ? {}
+            : {
+            time: +(new Date()),
+            data: deepClone(session)
+        };
+
+        this.session = session;
+        return this.session;
+    },
+
+    getTime: function() {
+        return this.session.time;
+    },
+
+    getData: function() {
+        return this.session.data;
+    },
+
+    getSession: function() {
+        return this.session;
+    },
+
+    getDiff: function(data) {
+        var diff = objectDiff.diff(this.getData(), data);
+        return {
+            diff: diff,
+            xml: objectDiff.convertToXMLString(diff)
+        };
+    }
+});
+
+module.exports = SaveState;
