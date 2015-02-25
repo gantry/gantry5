8ef9cb476a48a1d980d67d1d5dde6c623a735bde
Arifin FinLy <arifin@rockettheme.com>
2015-02-25 22:02:49 +0700

    Mega menu implementation for dropdown sub level 2+

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,13 +1,18 @@
 jQuery(document).ready(function($){
+	
 	// Open Dropdown
 	$('.g-menu-item').children('.g-menu-item-content').on('click', function(event){
-		event.preventDefault();
 		var selected = $(this);
 		if( selected.next('.g-dropdown').hasClass('g-inactive') ) {
 			selected.addClass('g-selected').next('.g-dropdown').removeClass('g-inactive').addClass('g-active').end().parent('li').parent('ul').addClass('g-slide-out');
 			selected.parent('.g-menu-item').siblings('.g-menu-item').children('ul').removeClass('g-active').addClass('g-inactive').end().children('.g-menu-item-content').removeClass('g-selected');
 		} else {
-			selected.removeClass('g-selected').next('.g-dropdown').addClass('g-inactive').removeClass('g-active').end().parent('li').parent('ul').removeClass('g-slide-out');
+			selected.removeClass('g-selected').next('.g-dropdown').removeClass('g-active').addClass('g-inactive').end().parent('li').parent('ul').removeClass('g-slide-out');
 		}
 	});
+
+	// SubMenu Item - Go Back Link
+	$('.g-go-back').on('click', function(){
+		$(this).closest('.g-dropdown').removeClass('g-active').addClass('g-inactive').closest('.g-sublevel').removeClass('g-slide-out');
+	});	
 });
