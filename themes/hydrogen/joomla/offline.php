763cdf2c8e17a6d63012ce21e0bbc6b79e090ef0
Matias Griese <matias@kunena.org>
2015-03-23 22:14:20 +0200

    Joomla: Implement offline page by grabbing content from templates/system/offline.php

--- themes/hydrogen/joomla/offline.php
+++ themes/hydrogen/joomla/offline.php
@@ -7,8 +7,14 @@ $gantry = include __DIR__ . '/includes/gantry.php';
 /** @var \Gantry\Framework\Theme $theme */
 $theme = $gantry['theme'];
 
+ob_start();
+include JPATH_THEMES . '/system/offline.php';
+$html = ob_get_clean();
+$start = strpos($html, '<body>') + 6;
+$end = strpos($html, '</body>', $start);
+
 $context = array(
-    'formtoken' => JHtml::_('form.token')
+    'message' => substr($html, $start, $end - $start)
 );
 
 // Render the page.
