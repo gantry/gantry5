a14a2152a53da957e8cce852e4be1bdd81373d25
Matias Griese <matias@kunena.org>
2015-04-14 19:44:21 +0300

    Fix fatal error if menu item doesn't have type set

--- src/classes/Gantry/Component/Menu/AbstractMenu.php
+++ src/classes/Gantry/Component/Menu/AbstractMenu.php
@@ -205,7 +205,6 @@ abstract class AbstractMenu implements \ArrayAccess, \Iterator
         if (isset($this->items[$item->parent_id])) {
             $this->items[$item->parent_id]->addChild($item);
         } else {
-            print_r($item->toArray());die();
             throw new \RuntimeException('Internal menu structure error');
         }
 
@@ -249,7 +248,7 @@ abstract class AbstractMenu implements \ArrayAccess, \Iterator
     {
         // Add custom menu elements.
         foreach ($items as $route => $item) {
-            if ($item['type'] != 'particle') {
+            if (!isset($item['type']) || $item['type'] != 'particle') {
                 continue;
             }
             $tree = explode('/', $route);
