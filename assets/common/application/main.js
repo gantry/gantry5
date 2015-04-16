cd9bc9037befa644d80bcfc9c0606367adeb27dc
Djamil Legato <djamil@djamil.it>
2015-04-16 15:27:03 -0700

    Temporary exposing `$` for development

--- assets/common/application/main.js
+++ assets/common/application/main.js
@@ -3,13 +3,15 @@
 var ready     = require('domready'),
     menu      = require('./menu'),
     offcanvas = require('./offcanvas'),
+    $         = require('./utils/dollar-extras'),
 
     instances = {};
 
 ready(function() {
     instances = {
         offcanvas: new offcanvas(),
-        menu: new menu()
+        menu: new menu(),
+        $: $
     };
 
     module.exports = window.G5 = instances;
