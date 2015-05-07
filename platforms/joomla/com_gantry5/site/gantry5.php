50769629cf23f167e70b0f11b04de7cf20705ef5
Matias Griese <matias@kunena.org>
2015-05-07 12:29:02 +0300

    Joomla: Handle error page inside menu

--- platforms/joomla/com_gantry5/site/gantry5.php
+++ platforms/joomla/com_gantry5/site/gantry5.php
@@ -1,2 +1,15 @@
 <?php
 defined('_JEXEC') or die;
+
+$app = JFactory::getApplication();
+$input = $app->input;
+
+// Prevent direct access without menu item.
+if (!$input->getInt('Itemid')) {
+    throw new RuntimeException(JText::_('JLIB_APPLICATION_ERROR_COMPONENT_NOT_FOUND'), 404);
+}
+
+// Handle error page.
+if ($input->getCmd('view') === 'error') {
+    throw new RuntimeException('Page not found', 404);
+}
