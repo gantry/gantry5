347e6d792fa59ce9cc344281a3ced168cb3c2bc6
Matias Griese <matias@kunena.org>
2015-05-11 10:29:55 +0300

    Joomla: Add copyright to all php files

--- platforms/joomla/com_gantry5/site/router.php
+++ platforms/joomla/com_gantry5/site/router.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @package   Gantry 5
+ * @author    RocketTheme http://www.rockettheme.com
+ * @copyright Copyright (C) 2007 - 2015 RocketTheme, LLC
+ * @license   GNU/GPLv2 and later
+ *
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
 defined('_JEXEC') or die ();
 
 function Gantry5BuildRoute(&$query)
