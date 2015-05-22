ac883865fac7e96581d57c2ad9ba4107f3a42cd5
Djamil Legato <djamil@djamil.it>
2015-05-22 15:08:50 -0700

    Particle Picker tweaks to allow Apply when field already set (part of #160)

--- platforms/common/js/main.js
+++ platforms/common/js/main.js
@@ -6762,10 +6762,10 @@ var $             = require('elements'),
     getAjaxSuffix = require('../../utils/get-ajax-suffix');
 
 
-ready(function(){
+ready(function() {
     var body = $('body');
 
-    body.delegate('click', '[data-g-instancepicker]', function(event, element){
+    body.delegate('click', '[data-g-instancepicker]', function(event, element) {
         if (event) { event.preventDefault(); }
 
         var data = JSON.parse(element.data('g-instancepicker')),
@@ -6782,16 +6782,71 @@ ready(function(){
             uri = value.type + '/' + value[data.type];
         }
 
+
         modal.open({
             content: 'Loading',
             method: !value || data.type == 'module' ? 'get' : 'post',
             data: !value || data.type == 'module' ? {} : value,
             remote: getAjaxURL(uri) + getAjaxSuffix(),
-            remoteLoaded: function(response, modal) {
-                var content = modal.elements.content,
+            remoteLoaded: function(response, modalInstance) {
+                var content = modalInstance.elements.content,
                     select = content.find('[data-mm-select]');
 
                 if (select) { select.data('g-instancepicker', element.data('g-instancepicker')); }
+                else {
+                    var form = content.find('form'),
+                        submit = content.find('input[type="submit"], button[type="submit"]'),
+                        dataString = [];
+
+                    if (!form || !submit) { return true; }
+
+                    var applyAndSave = content.search('[data-apply-and-save]');
+                    if (applyAndSave) { applyAndSave.remove(); }
+
+                    submit.on('click', function(e) {
+                        e.preventDefault();
+                        dataString = [];
+
+                        submit.showIndicator();
+
+                        $(form[0].elements).forEach(function(input) {
+                            input = $(input);
+                            var name = input.attribute('name'),
+                                value = input.value(),
+                                parent = input.parent('.settings-param'),
+                                override = parent ? parent.find('> input[type="checkbox"]') : null;
+
+                            if (!name || input.disabled() || (override && !override.checked())) { return; }
+                            dataString.push(name + '=' + encodeURIComponent(value));
+                        });
+
+                        var title = content.find('[data-title-editable]');
+                        if (title) {
+                            dataString.push('title=' + encodeURIComponent(title.data('title-editable')));
+                        }
+
+                        request(form.attribute('method'), form.attribute('action') + getAjaxSuffix(), dataString.join('&') || {}, function(error, response) {
+                            if (!response.body.success) {
+                                modal.open({
+                                    content: response.body.html || response.body,
+                                    afterOpen: function(container) {
+                                        if (!response.body.html) { container.style({ width: '90%' }); }
+                                    }
+                                });
+                            } else {
+
+                                var label = field.siblings('.g-instancepicker-title');
+
+                                if (field) { field.value(JSON.stringify(response.body.item)); }
+                                if (label) { label.text(response.body.item.title); }
+                            }
+
+                            modal.close();
+                            submit.hideIndicator();
+                        });
+                    });
+
+                }
             }
         });
     });
