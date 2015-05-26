d54db8bb46f736a9ce109f0424304428721b7ef1
Jakub Baran <jakub@rockettheme.com>
2015-05-26 22:57:52 +0200

    initial wp admin styling, gulp updates

--- gulpfile.js
+++ gulpfile.js
@@ -81,6 +81,11 @@ paths = {
             in: './engines/joomla/nucleus/scss/joomla.scss',
             out: './engines/joomla/nucleus/css-compiled/joomla.css',
             load: './engines/common/nucleus/scss'
+        },
+        { // nucleus - wordpress
+            in: './engines/wordpress/nucleus/scss/wordpress.scss',
+            out: './engines/wordpress/nucleus/css-compiled/wordpress.css',
+            load: './engines/common/nucleus/scss'
         }
     ],
     minify: [
