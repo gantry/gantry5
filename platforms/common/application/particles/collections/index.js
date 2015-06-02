2f0dfa602a1be8257c505e15113dcc3c6c5d7551
Djamil Legato <djamil@djamil.it>
2015-06-01 23:47:27 -0700

    Fixed show/hide of edit all button in collections (fixes #402)

--- platforms/common/application/particles/collections/index.js
+++ platforms/common/application/particles/collections/index.js
@@ -138,7 +138,7 @@ ready(function() {
         data.splice(index, 1);
         dataField.value(JSON.stringify(data));
         item.remove();
-        if (items.length) { list.parent().find('[data-collection-editall]').style('display', 'none'); }
+        if (items.length <= 2) { list.parent().find('[data-collection-editall]').style('display', 'none'); }
         body.emit('change', { target: dataField });
     });
 
