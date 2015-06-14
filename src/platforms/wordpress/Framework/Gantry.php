6398e9fdf7c81cb2201833b52b0a87bae2180e15
Jakub Baran <jakub@rockettheme.com>
2015-06-14 20:51:57 +0200

    changed the error message when Timber is not activated

--- src/platforms/wordpress/Framework/Gantry.php
+++ src/platforms/wordpress/Framework/Gantry.php
@@ -34,7 +34,7 @@ class Gantry extends Base\Gantry
     {
         // Make sure Timber plugin has been loaded.
         if ( !class_exists( 'Timber' ) ) {
-            throw new \LogicException( 'Timber not activated. Make sure you activate the plugin in <a href="' . admin_url( 'plugins.php#timber' ) . '">' . admin_url( 'plugins.php' ) . '</a>' );
+            throw new \LogicException( 'Timber not activated. Make sure you activate the plugin on the <a href="' . admin_url( 'plugins.php#timber' ) . '"><strong>Installed Plugins</strong></a> page.' );
         }
 
         $container = parent::load();
