7dabeab2bdef71882febcab347d75f8cce2edaa3
Jakub Baran <jakub@rockettheme.com>
2015-07-17 15:08:32 +0200

    some updates to the content views

--- themes/hydrogen/wordpress/single.php
+++ themes/hydrogen/wordpress/single.php
@@ -25,8 +25,4 @@ $context[ 'post' ] = $post;
 $context[ 'wp_title' ] .= ' - ' . $post->title();
 $context[ 'comment_form' ] = TimberHelper::get_comment_form();
 
-if ( post_password_required( $post->ID ) ) {
-	Timber::render( 'single-password.html.twig', $context );
-} else {
-	Timber::render( array( 'single-' . $post->ID . '.html.twig', 'single-' . $post->post_type . '.html.twig', 'single.html.twig' ), $context );
-}
+Timber::render( array( 'single-' . $post->ID . '.html.twig', 'single-' . $post->post_type . '.html.twig', 'single.html.twig' ), $context );
