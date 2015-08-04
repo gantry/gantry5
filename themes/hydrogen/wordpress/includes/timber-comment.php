6abb994ef4706b89a73ea6e35912a0a3598f4b89
Jakub Baran <jakub@rockettheme.com>
2015-08-04 11:16:45 +0200

    extra check for TimberComments class, check for the date and author not being null before displaying meta

--- themes/hydrogen/wordpress/includes/timber-comment.php
+++ themes/hydrogen/wordpress/includes/timber-comment.php
@@ -3,8 +3,10 @@
  * Helper class that allows us to get flat array of comments from TimberPost
  */
 
-class G5TimberComment extends TimberComment {
-    public function is_child() {
-        return false;
+if( class_exists( 'TimberComment' ) ) {
+    class G5TimberComment extends TimberComment {
+        public function is_child() {
+            return false;
+        }
     }
 }
