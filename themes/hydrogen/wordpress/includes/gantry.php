a8219d64651875dd2b5ee51e7220212666406e74
Jakub Baran <jakub@rockettheme.com>
2015-08-06 19:44:18 +0200

    when in the preview mode and Timber is not active, there will be a message saying what's causing it

--- themes/hydrogen/wordpress/includes/gantry.php
+++ themes/hydrogen/wordpress/includes/gantry.php
@@ -45,7 +45,10 @@ try {
     }
 
     add_filter( 'template_include', function() use ( $e ) {
-        echo 'Theme cannot be used. For more information, please login to administration.';
+        if( is_customize_preview() && !class_exists( 'Timber' ) ) {
+            _e( 'Timber library plugin not found. ', 'g5_hydrogen' );
+        }
+        _e( 'Theme cannot be used. For more information, please see the notice in administration.', 'g5_hydrogen' );
         die();
     });
 
