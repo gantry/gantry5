64e73d62ccd9690335a643a3365ba8bbd6fe5251
NewKind <newkind@gmail.com>
2015-08-28 21:45:27 +0200

    fixed typo

--- platforms/wordpress/gantry5/gantry5.php
+++ platforms/wordpress/gantry5/gantry5.php
@@ -42,7 +42,7 @@ function gantry5_plugin_defaults() {
         'production'       => '1',
         'debug'            => '0',
         'offline'          => '0',
-        'offline_message'  => 'Site is currently in the offline mode. Please try again later.',
+        'offline_message'  => 'Site is currently in offline mode. Please try again later.',
     );
 
     $option = get_option( 'gantry5_plugin' );
