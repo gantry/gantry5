01b52b004bd8738ee080b75ced802f13a70eed60
Djamil Legato <djamil@djamil.it>
2015-09-14 12:30:19 -0700

    Added support for widgets in Menu Editor and trash area. Fixed click issue on new blocks (fixes #828)

--- platforms/common/js/main.js
+++ platforms/common/js/main.js
@@ -4726,6 +4726,11 @@ var MenuManager = new prime({
     },
 
     click: function(event, element) {
+        var target = $(event.target);
+        if (target.matches('.g-menu-addblock') || target.parent('.g-menu-addblock')) {
+            return false;
+        }
+
         if (element.hasClass('g-block')) {
             this.stopAnimation();
             return true;
@@ -4812,7 +4817,7 @@ var MenuManager = new prime({
         }
 
         // it's a module or a particle and we allow for them to be deleted
-        if (!this.isNewParticle && (type && type.match(/__(module|particle)(-[a-z0-9]{5,})?$/i))) {
+        if (!this.isNewParticle && (type && type.match(/__(module|particle|widget)(-[a-z0-9]{5,})?$/i))) {
             this.eraser.show();
         }
 
@@ -4923,7 +4928,7 @@ var MenuManager = new prime({
 
         var target = event.type.match(/^touch/i) ? document.elementFromPoint(event.touches.item(0).clientX, event.touches.item(0).clientY) : event.target;
 
-        if (!this.isNewParticle && this.itemID.match(/__(module|particle)(-[a-z0-9]{5})?$/i)) {
+        if (!this.isNewParticle && this.itemID.match(/__(module|particle|widget)(-[a-z0-9]{5})?$/i)) {
             target = $(target);
             if (target.matches(this.eraser.element) || this.eraser.element.find(target)) {
                 this.dragdrop.removeElement = true;
