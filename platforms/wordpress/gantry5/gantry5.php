4decd6a8a8d65c0b9f5bc314ad70bcf4dbc827f6
Djamil Legato <djamil@djamil.it>
2015-09-21 23:16:49 -0700

    Whitespace Police :cop:

--- platforms/wordpress/gantry5/gantry5.php
+++ platforms/wordpress/gantry5/gantry5.php
@@ -12,7 +12,7 @@
  * Domain Path: /admin/languages
  */
 
-defined( 'ABSPATH' ) or die;
+defined('ABSPATH') or die;
 
 // NOTE: This file needs to be PHP 5.2 compatible.
 
@@ -26,42 +26,44 @@ if (version_compare(PHP_VERSION, '5.4.0', '<')) {
 
 require_once dirname(__FILE__) . '/src/Loader.php';
 
-if ( !defined( 'GANTRY5_PATH' ) ) {
+if (!defined('GANTRY5_PATH')) {
     // Works also with symlinks.
-    define( 'GANTRY5_PATH', rtrim( WP_PLUGIN_DIR, '/\\' ) . '/gantry5' );
+    define('GANTRY5_PATH', rtrim(WP_PLUGIN_DIR, '/\\') . '/gantry5');
 }
 
-if ( !is_admin() ) {
+if (!is_admin()) {
     return;
 }
 
-if ( !defined( 'GANTRYADMIN_PATH' ) ) {
+if (!defined('GANTRYADMIN_PATH')) {
     // Works also with symlinks.
-    define( 'GANTRYADMIN_PATH', GANTRY5_PATH . '/admin' );
+    define('GANTRYADMIN_PATH', GANTRY5_PATH . '/admin');
 }
 
 // Add Gantry 5 defaults on plugin activation
 // TODO: change the admin_init to a better hook ie. only when plugin updates
-register_activation_hook( __FILE__, 'gantry5_plugin_defaults' );
-add_action( 'admin_init', 'gantry5_plugin_defaults' );
+register_activation_hook(__FILE__, 'gantry5_plugin_defaults');
+add_action('admin_init', 'gantry5_plugin_defaults');
 
-function gantry5_plugin_defaults() {
+function gantry5_plugin_defaults()
+{
     $defaults = array(
-        'production'       => '1',
-        'debug'            => '0',
-        'offline'          => '0',
-        'offline_message'  => 'Site is currently in offline mode. Please try again later.',
-        'cache_path'       => '',
+        'production'      => '1',
+        'debug'           => '0',
+        'offline'         => '0',
+        'offline_message' => 'Site is currently in offline mode. Please try again later.',
+        'cache_path'      => '',
     );
 
-    $option = (array) get_option( 'gantry5_plugin' );
+    $option = (array)get_option('gantry5_plugin');
 
-    update_option( 'gantry5_plugin', $option + $defaults );
+    update_option('gantry5_plugin', $option + $defaults);
 }
 
-add_filter( 'kses_allowed_protocols', 'add_gantry5_streams_to_kses' );
+add_filter('kses_allowed_protocols', 'add_gantry5_streams_to_kses');
 
-function add_gantry5_streams_to_kses( $protocols ) {
+function add_gantry5_streams_to_kses($protocols)
+{
     $streams = array(
         'gantry-cache',
         'gantry-themes',
@@ -78,30 +80,32 @@ function add_gantry5_streams_to_kses( $protocols ) {
         'wp-content',
     );
 
-    $protocols = array_merge( $protocols, $streams );
+    $protocols = array_merge($protocols, $streams);
     return $protocols;
 }
 
 // Initialize plugin language and fallback to en_US if the .mo file can't be found
-$domain = 'gantry5';
-$languages_path = basename( GANTRY5_PATH ) . '/admin/languages';
+$domain         = 'gantry5';
+$languages_path = basename(GANTRY5_PATH) . '/admin/languages';
 
-if( load_plugin_textdomain( $domain, false, $languages_path ) === false ) {
-    add_filter( 'plugin_locale', 'modify_gantry5_locale', 10, 2 );
+if (load_plugin_textdomain($domain, false, $languages_path) === false) {
+    add_filter('plugin_locale', 'modify_gantry5_locale', 10, 2);
 }
 
-load_plugin_textdomain( $domain, false, $languages_path );
+load_plugin_textdomain($domain, false, $languages_path);
 
-function modify_gantry5_locale( $locale, $domain ) {
+function modify_gantry5_locale($locale, $domain)
+{
     // Revert the gantry5 domain locale to en_US
-    if( isset( $domain ) && $domain == 'gantry5' ) {
+    if (isset($domain) && $domain == 'gantry5') {
         $locale = 'en_US';
     }
 
     return $locale;
 }
 
-function gantry5_php_version_warning() {
+function gantry5_php_version_warning()
+{
     echo '<div class="error"><p>';
     echo sprintf("You are running PHP %s, but Gantry 5 Framework needs at least PHP %s to run.", PHP_VERSION, '5.4.0');
     echo '</p></div>';
