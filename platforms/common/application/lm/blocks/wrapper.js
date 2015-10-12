d4494064b8bf8c49d84981affd1d52d736e05bd8
Djamil Legato <djamil@djamil.it>
2015-10-12 16:41:48 -0700

    Layout: Implemented support for Wrapper

---
+++ platforms/common/application/lm/blocks/wrapper.js
@@ -0,0 +1,28 @@
+"use strict";
+var prime   = require('prime'),
+    Section = require('./section'),
+
+    getAjaxURL = require('../../utils/get-ajax-url').config;
+
+var Wrapper = new prime({
+    inherits: Section,
+    options: {
+        type: 'wrapper',
+        attributes: {
+            name: "Wrapper"
+        }
+    },
+
+    layout: function() {
+        var settings_uri = getAjaxURL(this.getPageId() + '/layout/' + this.getType() + '/' + this.getId());
+        return '<div class="wrapper-section" data-lm-id="' + this.getId() + '" data-lm-blocktype="' + this.getType() + '"></div>';
+    },
+
+    hasChanged: function() {},
+
+    getId: function() {
+        return this.id || (this.id = this.options.type);
+    }
+});
+
+module.exports = Wrapper;
