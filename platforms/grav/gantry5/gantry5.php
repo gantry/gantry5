26a3f908fcb5e08e76a368d65ba7635bbb1267f7
Matias Griese <matias@kunena.org>
2015-11-25 19:38:22 +0200

    Grav: Initial admin works

--- platforms/grav/gantry5/gantry5.php
+++ platforms/grav/gantry5/gantry5.php
@@ -132,7 +132,7 @@ class Gantry5Plugin extends Plugin
         }
 
         if (!defined('GANTRYADMIN_PATH')) {
-            define('GANTRYADMIN_PATH', __DIR__);
+            define('GANTRYADMIN_PATH', 'plugins://gantry5/admin');
         }
 
         $base = rtrim($this->grav['base_url'], '/');
@@ -182,13 +182,16 @@ class Gantry5Plugin extends Plugin
         $page->init(new \SplFileInfo(__DIR__ . "/pages/gantry.md"));
         $page->slug($this->template);
 
+        // Dispatch Gantry in output buffer.
         ob_start();
         $gantry = Gantry::instance();
         $gantry['router']->dispatch();
         $content = ob_get_clean();
 
+        // Store response into the page.
         $page->content($content);
 
+        // Hook page into Grav as current page.
         $this->grav['page'] = function () use ($page) { return $page; };
     }
 
@@ -201,12 +204,13 @@ class Gantry5Plugin extends Plugin
         $twig = $this->grav['twig'];
 
         /** @var UniformResourceLocator $locator */
+        // TODO: get rid of these and use the ones in admin template class.
         $locator = $this->grav['locator'];
-        $locator->addPath('gantry-admin', '', ['plugins://gantry5', 'plugins://gantry5/common']);
-        $locator->addPath('gantry-admin', 'assets', ['plugins://gantry5/common']);
+        $locator->addPath('gantry-admin', '', ['plugins://gantry5/admin', 'plugins://gantry5/admin/common']);
+        $locator->addPath('gantry-admin', 'assets', ['plugins://gantry5/admin/common']);
 
         $loader = $twig->loader();
-        $loader->setPaths($locator->findResources('gantry-admin://templates'), 'gantry-admin');
+        $loader->prependPath($locator->findResource('plugins://gantry5/templates'));
     }
 
     /**
@@ -217,7 +221,7 @@ class Gantry5Plugin extends Plugin
         /** @var Twig $twig */
         $twig = $this->grav['twig'];
 
-        $twig->template = "@gantry-admin/pages/about/{$this->template}.html.twig";
+        //$twig->template = "@gantry-admin/pages/about/{$this->template}.html.twig";
 
         $twig->twig_vars['location'] = $this->template;
         $twig->twig_vars['gantry_url'] = $this->base;
--- platforms/grav/gantry5/pages/gantry.md
+++ platforms/grav/gantry5/pages/gantry.md
@@ -1,6 +1,11 @@
 ---
 title: Gantry Templates
 
+cache_enable: false
+process:
+    markdown: false
+    twig: false
+    
 access:
     admin.themes: true
     admin.super: true
