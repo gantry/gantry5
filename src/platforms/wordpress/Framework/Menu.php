5a62ea0a71ffbe02bcbea8067d6a19cd15483191
Matias Griese <matias@kunena.org>
2015-12-17 12:04:51 +0200

    WordPress: Fixed Base Item in Menu particle being empty (#1033)

--- src/platforms/wordpress/Framework/Menu.php
+++ src/platforms/wordpress/Framework/Menu.php
@@ -10,6 +10,7 @@
 
 namespace Gantry\Framework;
 
+use Gantry\Component\Config\Config;
 use Gantry\Component\Gantry\GantryTrait;
 use Gantry\Component\Menu\AbstractMenu;
 use Gantry\Component\Menu\Item;
