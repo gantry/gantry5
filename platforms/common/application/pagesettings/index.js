gantry/gantry5

commit 9521bd4173812d07221109c0a368e5666d8a9744

Fixed issue that was preventing Atoms from properly getting sorted and deleted on touch devices (fixes #1259)

platforms/common/application/pagesettings/index.js
@@ -85,9 +85,21 @@ var Atoms = {
                 },
 
                 onEnd: function(event) {
-                    var item = $(event.item);
-                    var target = $(this.originalEvent.target);
-                    if (target.matches('#trash') || target.parent('#trash')) {
+                    var item = $(event.item),
+                        trash = $('#trash'),
+                        target = $(this.originalEvent.target),
+                        touchTrash = false;
+
+                    // workaround for touch devices
+                    if (this.originalEvent.type === 'touchend') {
+                        var trashSize = trash[0].getBoundingClientRect(),
+                            oE        = this.originalEvent,
+                            position  = (oE.pageY || oE.changedTouches[0].pageY) - window.scrollY;
+
+                        touchTrash = position <= trashSize.height;
+                    }
+
+                    if (target.matches('#trash') || target.parent('#trash') || touchTrash) {
                         item.remove();
                         Atoms.eraser.hide();
                         this.options.onSort();
@@ -248,11 +260,18 @@ var AttachSettings = function() {
     });
 };
 
+var AttachSortableAtoms = function(atoms) {
+    if (!atoms.SimpleSort) { Atoms.createSortables(atoms); }
+};
+
 ready(function() {
+    var atoms = $('#atoms');
+
     $('body').delegate('mouseover', '#atoms', function(event, element) {
-        if (!element.SimpleSort) { Atoms.createSortables(element); }
+        AttachSortableAtoms(atoms);
     });
 
+    AttachSortableAtoms(atoms);
     AttachSettings();
 });
 

platforms/common/js/main.js
@@ -5479,9 +5479,21 @@ var Atoms = {
                 },
 
                 onEnd: function(event) {
-                    var item = $(event.item);
-                    var target = $(this.originalEvent.target);
-                    if (target.matches('#trash') || target.parent('#trash')) {
+                    var item = $(event.item),
+                        trash = $('#trash'),
+                        target = $(this.originalEvent.target),
+                        touchTrash = false;
+
+                    // workaround for touch devices
+                    if (this.originalEvent.type === 'touchend') {
+                        var trashSize = trash[0].getBoundingClientRect(),
+                            oE        = this.originalEvent,
+                            position  = (oE.pageY || oE.changedTouches[0].pageY) - window.scrollY;
+
+                        touchTrash = position <= trashSize.height;
+                    }
+
+                    if (target.matches('#trash') || target.parent('#trash') || touchTrash) {
                         item.remove();
                         Atoms.eraser.hide();
                         this.options.onSort();
@@ -5642,11 +5654,18 @@ var AttachSettings = function() {
     });
 };
 
+var AttachSortableAtoms = function(atoms) {
+    if (!atoms.SimpleSort) { Atoms.createSortables(atoms); }
+};
+
 ready(function() {
+    var atoms = $('#atoms');
+
     $('body').delegate('mouseover', '#atoms', function(event, element) {
-        if (!element.SimpleSort) { Atoms.createSortables(element); }
+        AttachSortableAtoms(atoms);
     });
 
+    AttachSortableAtoms(atoms);
     AttachSettings();
 });
 

commit 894e748f156f080e8d28da142fc3c9717c703c5b

Fixed issue introduced with 9521bd417 and Atoms

platforms/common/application/pagesettings/index.js
@@ -261,6 +261,7 @@ var AttachSettings = function() {
 };
 
 var AttachSortableAtoms = function(atoms) {
+    if (!atoms) { return; }
     if (!atoms.SimpleSort) { Atoms.createSortables(atoms); }
 };
 
@@ -268,7 +269,7 @@ ready(function() {
     var atoms = $('#atoms');
 
     $('body').delegate('mouseover', '#atoms', function(event, element) {
-        AttachSortableAtoms(atoms);
+        AttachSortableAtoms(element);
     });
 
     AttachSortableAtoms(atoms);

platforms/common/js/main.js
@@ -5655,6 +5655,7 @@ var AttachSettings = function() {
 };
 
 var AttachSortableAtoms = function(atoms) {
+    if (!atoms) { return; }
     if (!atoms.SimpleSort) { Atoms.createSortables(atoms); }
 };
 
@@ -5662,7 +5663,7 @@ ready(function() {
     var atoms = $('#atoms');
 
     $('body').delegate('mouseover', '#atoms', function(event, element) {
-        AttachSortableAtoms(atoms);
+        AttachSortableAtoms(element);
     });
 
     AttachSortableAtoms(atoms);