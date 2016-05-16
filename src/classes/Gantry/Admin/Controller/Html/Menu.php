d0be7db291b91061476dfa91eaf727014a115fbe
Matias Griese <matias@kunena.org>
2016-05-16 12:03:25 +0300

    WordPress: Fix loading/saving menu items with entities (#1248)

--- src/classes/Gantry/Admin/Controller/Html/Menu.php
+++ src/classes/Gantry/Admin/Controller/Html/Menu.php
@@ -188,7 +188,7 @@ class Menu extends HtmlController
             return call_user_func_array([$this, 'validateitem'], $params);
         }
 
-        $path = implode('/', $path);
+        $path = html_entity_decode(implode('/', $path), ENT_COMPAT | ENT_HTML5, 'UTF-8');
 
         // Load the menu.
         $resource = $this->loadResource($id);
