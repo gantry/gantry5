0453c8fc6b08c6f119114da922fe56a1d255d6a5
Matias Griese <matias@kunena.org>
2016-06-06 14:31:25 +0300

    Joomla: Fix saving menu title (#1176)

--- src/platforms/joomla/Admin/EventListener.php
+++ src/platforms/joomla/Admin/EventListener.php
@@ -151,7 +151,6 @@ class EventListener implements EventSubscriberInterface
                 $browserNav = intval($menu["items.{$key}.target"] === '_blank');
 
                 $options = [
-                    'menu-anchor_title' => $menu["items.{$key}.subtitle"],
                     'menu-anchor_css' => $menu["items.{$key}.anchor_class"],
                     'menu_image' => $menu["items.{$key}.image"],
                     'menu_text' => intval(!$menu["items.{$key}.icon_only"]),
