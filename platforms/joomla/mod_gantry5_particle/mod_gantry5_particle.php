61e925848a81664cff87b8b7d7ca019a79724ca0
Matias Griese <matias@kunena.org>
2016-06-30 15:39:19 +0300

    Fix fatal error if there are more than one module (#977)

--- platforms/joomla/mod_gantry5_particle/mod_gantry5_particle.php
+++ platforms/joomla/mod_gantry5_particle/mod_gantry5_particle.php
@@ -19,6 +19,8 @@ if (!class_exists('Gantry\Framework\Gantry')) {
     return;
 }
 
+include_once dirname(__FILE__) . '/helper.php';
+
 /** @var object $params */
 
 $gantry = \Gantry\Framework\Gantry::instance();
@@ -51,55 +53,3 @@ $document->appendHeaderTags($assets);
 echo $html;
 
 GANTRY_DEBUGGER && \Gantry\Debugger::stopTimer("module-{$module->id}");
-
-
-class ModGantryParticlesHelper
-{
-    /**
-     * @param object $module
-     * @param object $params
-     * @return array
-     */
-    public static function render($module, $params)
-    {
-        GANTRY_DEBUGGER && \Gantry\Debugger::addMessage("Particle Module #{$module->id} was not cached");
-
-        $data = json_decode($params->get('particle'), true);
-        $type = $data['type'];
-        $particle = $data['particle'];
-
-        $gantry = \Gantry\Framework\Gantry::instance();
-        if ($gantry->debug()) {
-            $enabled_outline = $gantry['config']->get("particles.{$particle}.enabled", true);
-            $enabled = isset($data['options']['particle']['enabled']) ? $data['options']['particle']['enabled'] : true;
-            $location = (!$enabled_outline ? 'Outline' : (!$enabled ? 'Module' : null));
-
-            if ($location) {
-                return ['<div class="alert alert-error">The Particle has been disabled from the ' . $location . ' and won\'t render.</div>', []];
-            }
-        }
-
-        $context = array(
-            'gantry' => $gantry,
-            'inContent' => true,
-            'segment' => array(
-                'id' => "module-{$particle}-{$module->id}",
-                'type' => $type,
-                'subtype' => $particle,
-                'attributes' => $data['options']['particle'],
-            )
-        );
-
-        /** @var \Gantry\Framework\Document $document */
-        $document = $gantry['document'];
-        $document->push();
-
-        /** @var Gantry\Framework\Theme $theme */
-        $theme = $gantry['theme'];
-        $html = trim($theme->render("@nucleus/content/particle.html.twig", $context));
-
-        $assets = $document->pop();
-
-        return [$html, $assets];
-    }
-}
