11da7f0ca1ed05306bf87aaef2515e0b62fc8084
Matias Griese <matias@kunena.org>
2016-12-08 12:43:01 +0200

    YAML syntax fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@
         - Added Lightbox support by creating `Lightcase Atom`. By default this feature can be used by adding `data-rel="lightcase"` into the link
     3. [](#bugfix)
         - Fix broken responsive support classes (#1705)
+        - YAML syntax fixes
 2. [Joomla](#joomla)
     2. [](#improved)
         - Improved outline assignments logic
--- src/classes/Gantry/Component/File/CompiledFile.php
+++ src/classes/Gantry/Component/File/CompiledFile.php
@@ -62,61 +62,66 @@ trait CompiledFile
             throw new \BadMethodCallException("Cache path not defined for compiled file ({$this->filename})!");
         }
 
-        // If nothing has been loaded, attempt to get pre-compiled version of the file first.
-        if ($var === null && $this->raw === null && $this->content === null) {
-            $modified = $this->modified();
+        try {
+            // If nothing has been loaded, attempt to get pre-compiled version of the file first.
+            if ($var === null && $this->raw === null && $this->content === null) {
+                $modified = $this->modified();
 
-            if (!$modified || !$this->caching) {
-                return $this->decode($this->raw());
-            }
-
-            $key = md5($this->filename);
-            $file = PhpFile::instance($this->cachePath . "/{$key}{$this->extension}.php");
-
-            $class = get_class($this);
-
-            $cache = $file->exists() ? $file->content() : null;
-
-            // Load real file if cache isn't up to date (or is invalid).
-            if (!isset($cache['@class'])
-                || $cache['@class'] != $class
-                || $cache['modified'] != $modified
-                || $cache['filename'] != $this->filename
-            ) {
-                // Attempt to lock the file for writing.
-                try {
-                    $file->lock(false);
-                } catch (\Exception $e) {
-                    // Another process has locked the file; we will check this in a bit.
+                if (!$modified || !$this->caching) {
+                    return $this->decode($this->raw());
                 }
 
-                // Decode RAW file into compiled array.
-                $data = $this->decode($this->raw());
-                $cache = [
-                    '@class' => $class,
-                    'filename' => $this->filename,
-                    'modified' => $modified,
-                    'data' => $data
-                ];
+                $key = md5($this->filename);
+                $file = PhpFile::instance($this->cachePath . "/{$key}{$this->extension}.php");
 
-                // If compiled file wasn't already locked by another process, save it.
-                if ($file->locked() !== false) {
-                    $file->save($cache);
-                    $file->unlock();
+                $class = get_class($this);
 
-                    // Compile cached file into bytecode cache
-                    if (function_exists('opcache_invalidate')) {
-                        // Silence error in case if `opcache.restrict_api` directive is set.
-                        @opcache_invalidate($file->filename(), true);
-                    } elseif (function_exists('apc_compile_file')) {
-                        // PHP 5.4
-                        @apc_compile_file($file->filename());
+                $cache = $file->exists() ? $file->content() : null;
+
+                // Load real file if cache isn't up to date (or is invalid).
+                if (!isset($cache['@class'])
+                    || $cache['@class'] != $class
+                    || $cache['modified'] != $modified
+                    || $cache['filename'] != $this->filename
+                ) {
+                    // Attempt to lock the file for writing.
+                    try {
+                        $file->lock(false);
+                    } catch (\Exception $e) {
+                        // Another process has locked the file; we will check this in a bit.
+                    }
+
+                    // Decode RAW file into compiled array.
+                    $data = $this->decode($this->raw());
+                    $cache = [
+                        '@class' => $class,
+                        'filename' => $this->filename,
+                        'modified' => $modified,
+                        'data' => $data
+                    ];
+
+                    // If compiled file wasn't already locked by another process, save it.
+                    if ($file->locked() !== false) {
+                        $file->save($cache);
+                        $file->unlock();
+
+                        // Compile cached file into bytecode cache
+                        if (function_exists('opcache_invalidate')) {
+                            // Silence error in case if `opcache.restrict_api` directive is set.
+                            @opcache_invalidate($file->filename(), true);
+                        } elseif (function_exists('apc_compile_file')) {
+                            // PHP 5.4
+                            @apc_compile_file($file->filename());
+                        }
                     }
                 }
-            }
-            $file->free();
+                $file->free();
 
-            $this->content = $cache['data'];
+                $this->content = $cache['data'];
+            }
+
+        } catch (\Exception $e) {
+            throw new \RuntimeException(sprintf('Failed to read %s: %s', basename($this->filename), $e->getMessage()), 500, $e);
         }
 
         return parent::content($var);
