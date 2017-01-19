103e74e142a53a55fc41a4db95f15be43f013c4c
Matias Griese <matias@kunena.org>
2017-01-19 09:14:44 +0200

    Grav: Fix offline mode part 2 (#1808)

--- platforms/grav/gantry5/gantry5.php
+++ platforms/grav/gantry5/gantry5.php
@@ -314,7 +314,7 @@ class Gantry5Plugin extends Plugin
         if ($gantry['global']->get('offline', 0)) {
             GANTRY_DEBUGGER && \Gantry\Debugger::addMessage("Site is Offline!");
 
-            if (!isset($this->grav['user']->authenticated)) {
+            if (empty($this->grav['user']->authenticated)) {
                 $page = new Page;
                 $page->init(new \SplFileInfo(__DIR__ . '/pages/offline.md'));
 
