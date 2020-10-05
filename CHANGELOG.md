6aaf9926569629be29f3869bcb363c514079e4fd
Matias Griese <matias@trilbymedia.com>
2020-10-05 12:37:46 +0300

    Added configuration option to support Advance Module Manager (#2710)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # 5.4.36
 ## mm/dd/2020
 
+1. [Joomla](#joomla)
+    1. [](#new)
+        - Added configuration option to support Advance Module Manager (#2710)
 1. [Grav](#grav)
     1. [](#bugfix)
         - Fixed `{{ gantry.platform.filter() }}` throws error (#2700)
---
+++ platforms/joomla/plg_system_gantry5/fields/warning.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * @package   Gantry 5
+ * @author    RocketTheme http://www.rockettheme.com
+ * @copyright Copyright (C) 2007 - 2020 RocketTheme, LLC
+ * @license   GNU/GPLv2 and later
+ *
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+class JFormFieldWarning extends JFormField
+{
+    protected $type = 'Warning';
+
+    protected function getLabel()
+    {
+        return 'Gantry 5';
+    }
+
+    protected function getInput()
+    {
+        $app = JFactory::getApplication();
+        $input = $app->input;
+
+        $route = '';
+        $cid = $input->post->get('cid', (array) $input->getInt('id'), 'array');
+        if ($cid) {
+            $styles = $this->getStyles();
+            $selected = array_intersect_key($styles, array_flip($cid));
+            if ($selected) {
+                $theme = reset($selected);
+                $id = key($selected);
+                $token = JSession::getFormToken();
+                $route = "index.php?option=com_gantry5&view=configurations/{$id}";
+            }
+        }
+
+        if (!$route) {
+            return '<a href="index.php?option=com_gantry5" class="btn" style="background:#439a86; color:#fff;">Gantry 5</a>';
+        }
+
+        $lang = JFactory::getLanguage();
+        $lang->load('com_gantry5', JPATH_ADMINISTRATOR) || $lang->load('com_gantry5', JPATH_ADMINISTRATOR . '/components/com_gantry5');
+
+        $title1 = JText::_('GANTRY5_PLATFORM_STYLES');
+        $title2 = JText::_('GANTRY5_PLATFORM_LAYOUT');
+        $title3 = JText::_('GANTRY5_PLATFORM_PAGESETTINGS');
+
+        return <<<HTML
+<a href="{$route}/styles&theme={$theme}&{$token}=1" class="btn" style="background:#439a86; color:#fff;">{$title1}</a>
+<a href="{$route}/layout&theme={$theme}&{$token}=1" class="btn" style="background:#439a86; color:#fff;">{$title2}</a>
+<a href="{$route}/page&theme={$theme}&{$token}=1" class="btn" style="background:#439a86; color:#fff;">{$title3}</a>
+HTML;
+    }
+
+    /**
+     * @return array
+     */
+    private function getStyles()
+    {
+        static $list;
+
+        if ($list === null) {
+            // Load styles
+            $db = JFactory::getDbo();
+            $query = $db
+                ->getQuery(true)
+                ->select('s.id, s.template')
+                ->from('#__template_styles as s')
+                ->where('s.client_id = 0')
+                ->where('e.enabled = 1')
+                ->leftJoin('#__extensions as e ON e.element=s.template AND e.type=' . $db->quote('template') . ' AND e.client_id=s.client_id');
+
+            $db->setQuery($query);
+            $templates = (array)$db->loadObjectList();
+
+            $list = array();
+
+            foreach ($templates as $template) {
+                if ($this->isGantryTemplate($template->template)) {
+                    $list[$template->id] = $template->template;
+                }
+            }
+        }
+
+        return $list;
+    }
+
+    private function isGantryTemplate($name)
+    {
+        return file_exists(JPATH_SITE . "/templates/{$name}/gantry/theme.yaml");
+    }
+}
