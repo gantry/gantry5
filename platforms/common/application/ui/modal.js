05ad08649566f871f61a37c76418284a992c8053
Karol Orzeł <karol@rockettheme.com>
2021-08-24 12:37:24 +0200

    Wordpress 5.8 Fixes

--- platforms/common/application/ui/modal.js
+++ platforms/common/application/ui/modal.js
@@ -205,7 +205,8 @@ var Modal = new prime({
 
         // wordpress workaround for out-of-scope cases
         if (GANTRY_PLATFORM == 'wordpress') {
-            container = $('#customize-preview') || $('#widgets-right') || $(options.appendNode);
+            container = $('#widgets-editor') || $('#widgets-right') || $(options.appendNode);
+            console.log(container);
             if ('#' + container.id() != options.appendNode) {
                 var wpwrap = $('#wpwrap') || $('.wp-customizer'), sibling, workaround;
                 if (wpwrap.id() == 'wpwrap') {
