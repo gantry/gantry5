5dc9c3ddc911e901791903f1242db593c330c883
Matias Griese <matias@trilbymedia.com>
2022-03-28 15:45:14 +0300

    Moved fix methods

--- src/platforms/joomla/classes/Gantry/Joomla/Content/Content.php
+++ src/platforms/joomla/classes/Gantry/Joomla/Content/Content.php
@@ -261,4 +261,19 @@ class Content extends AbstractObject
     {
         return $this->getCreateSql(['asset_id']) . ';';
     }
+
+    protected function fixValue($table, $k, $v)
+    {
+        // Prepare and sanitize the fields and values for the database query.
+        if (in_array($k, ['checked_out', 'created_by', 'modified_by'], true) && !$v) {
+            $v = '@null_user';
+        } elseif (in_array($k, ['checked_out_time', 'publish_up', 'publish_down', 'created', 'created_time', 'modified_time'], true) && ($v === null || $v === '0000-00-00 00:00:00')) {
+            $v = '@null_time';
+        } elseif (is_string($v)) {
+            $dbo = $table->getDbo();
+            $v = $dbo->quote($v);
+        }
+
+        return $v;
+    }
 }
--- src/platforms/joomla/classes/Gantry/Joomla/Object/AbstractObject.php
+++ src/platforms/joomla/classes/Gantry/Joomla/Object/AbstractObject.php
@@ -419,20 +419,21 @@ abstract class AbstractObject extends \JObject
                 $v = json_encode($v);
             }
 
-            // Prepare and sanitize the fields and values for the database query.
-            if (in_array($k, ['checked_out', 'created_user_id', 'modified_user_id', 'created_by', 'modified_by'], true) && !$v) {
-                $v = '@null_user';
-            } elseif (in_array($k, ['checked_out_time', 'publish_up', 'publish_down', 'created', 'created_time', 'modified_time'], true) && ($v === null || $v === '0000-00-00 00:00:00')) {
-                $v = '@null_time';
-            } elseif (is_string($v)) {
-                $v = $dbo->quote($v);
-            }
-
             $k = $dbo->quoteName($k);
-            $values[$k] = $v;
+            $values[$k] = $this->fixValue($table, $k, $v);
         }
 
         return $values;
+    }
+
+    protected function fixValue($table, $k, $v)
+    {
+        if (is_string($v)) {
+            $dbo = $table->getDbo();
+            $v = $dbo->quote($v);
+        }
+
+        return $v;
     }
 
     /**
