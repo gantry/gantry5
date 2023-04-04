534c220fe359af1831c6f38e70f37b0f065ab395
Karol Orzeł <hexplor@gmail.com>
2023-04-04 12:37:51 +0200

    Release 5.5.16 - Automatic Version Number Updates(Attempt 2)

--- src/RealLoader.php
+++ src/RealLoader.php
@@ -49,8 +49,8 @@ abstract class RealLoader
             throw new \LogicException(self::$errorMessageGantryLoaded);
         }
 
-        define('GANTRY5_VERSION', '@version@');
-        define('GANTRY5_VERSION_DATE', '@versiondate@');
+        define('GANTRY5_VERSION', '5.5.16');
+        define('GANTRY5_VERSION_DATE', 'April 4, 2023');
 
         if (!defined('DS')) {
             define('DS', DIRECTORY_SEPARATOR);
