49979d4c9415728c16fd70b40dc41abbe44e898c
Monika <t12titans@gmail.com>
2025-09-15 15:57:06 +0530

    Release 5.5.25

--- src/RealLoader.php
+++ src/RealLoader.php
@@ -51,8 +51,8 @@ abstract class RealLoader
             throw new \LogicException(self::$errorMessageGantryLoaded);
         }
 
-        define('GANTRY5_VERSION', '5.5.19');
-        define('GANTRY5_VERSION_DATE', 'May 24, 2024');
+        define('GANTRY5_VERSION', '5.5.25');
+        define('GANTRY5_VERSION_DATE', 'Sep 15, 2025');
 
         if (!defined('DS')) {
             define('DS', DIRECTORY_SEPARATOR);
