3291508ce0cdcfc0de927cd20f3d50e48ab8591c
Monika <t12titans@gmail.com>
2026-04-09 16:54:11 +0530

    Fixed static version to dynamic

--- src/RealLoader.php
+++ src/RealLoader.php
@@ -51,8 +51,8 @@ abstract class RealLoader
             throw new \LogicException(self::$errorMessageGantryLoaded);
         }
 
-        define('GANTRY5_VERSION', '5.6.1');
-        define('GANTRY5_VERSION_DATE', 'Apr 08, 2026');
+        define('GANTRY5_VERSION', '@version@');
+        define('GANTRY5_VERSION_DATE', '@versiondate@');
 
         if (!defined('DS')) {
             define('DS', DIRECTORY_SEPARATOR);
